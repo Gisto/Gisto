513da188b305eac480391540ff2029c665794d05
Sasha Khamkov <sanusart@gmail.com>
2013-03-31 23:19:20 +0300

    Minor additions and css fixes

--- app/js/app.js
+++ app/js/app.js
@@ -4,7 +4,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/none', {templateUrl: 'partials/empty.html'});
+    $routeProvider.when('/', {templateUrl: 'partials/empty.html'});
     $routeProvider.when('/gist/:gistId', {templateUrl: 'partials/single-gist.html', controller: theGist});
     $routeProvider.otherwise({redirectTo: '/none'});
   }]);
