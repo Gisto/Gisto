c4a0276674de4308c3010ae86943bb421d0817a5
Sasha Khamkov <sanusart@gmail.com>
2013-04-02 09:50:00 +0300

    Fix editor directive to get file language from gist. CSS fixes.

--- app/js/directives.js
+++ app/js/directives.js
@@ -5,35 +5,21 @@
 var app = angular.module('myApp.directives', []);
 
 
- app.directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
+app.directive('appVersion', ['version', function(version) {
+        return function(scope, elm, attrs) {
+            elm.text(version);
+        };
+    }]);
+
+app.directive('editor', function($timeout) {
+    return function(scope, element, attrs) {
+        $timeout(function() {
+            var lang = attrs.editor;
+            console.log('Lang to highlight: ' + lang);
+
+            var editor = ace.edit(element[0]);
+            editor.setTheme("ace/theme/textmate");
+            editor.getSession().setMode("ace/mode/" + lang);
+        }, 0);
     };
-  }]);
-
- app.directive('editor', function($timeout) {
- 	return function(scope, element, attrs) {
- 		$timeout(function() {
- 			var extension = attrs.editor.split('.').pop();
- 			console.log(getLanguage(extension));
-
-
- 			var editor = ace.edit(element[0]);
-			editor.setTheme("ace/theme/textmate");
-			editor.getSession().setMode("ace/mode/" + getLanguage(extension));
-		},0);
- 		
- 	};
 });
-
- var getLanguage = function(ext) {
- 	console.log('getLanguage');
-
- 	if (ext === 'cs') {
- 		return 'csharp';
- 	} else if (ext === 'js') {
- 		return 'javascript';
- 	} else {
- 		return ext;
- 	}
- }
