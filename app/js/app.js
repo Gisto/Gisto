bbbd2dacc4eb6f290b05d9626be0612234867364
Sasha Khamkov <sanusart@gmail.com>
2013-04-04 01:48:06 +0300

    Loading on login + css fixes

--- app/js/app.js
+++ app/js/app.js
@@ -5,7 +5,7 @@
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'JobIndicator']).
         config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/', {templateUrl: 'partials/empty.html'});
-        $routeProvider.when('/gist/:gistId', {templateUrl: 'partials/single-gist.html', controller: theGist});
+        $routeProvider.when('/gist/:gistId', {templateUrl: 'partials/single-gist.html', controller: singleGistCtrl});
         $routeProvider.when('/create', {templateUrl: 'partials/create.html', controller: createGistCtrl});
         $routeProvider.otherwise({redirectTo: '/'});
     }]);
--- app/js/main.js
+++ app/js/main.js
@@ -38,5 +38,4 @@ $(function() {
             console.log("success");
         });
     });
-
 });
