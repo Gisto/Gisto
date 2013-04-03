9a5af9b0ea3e6c988fd32c5e25dd0b2abb636aed
Sasha Khamkov <sanusart@gmail.com>
2013-04-03 22:02:20 +0300

    Created working indication (in service.js) - OOPS!

--- app/js/app.js
+++ app/js/app.js
@@ -9,37 +9,3 @@ angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 
         $routeProvider.when('/create', {templateUrl: 'partials/create.html', controller: createGistCtrl});
         $routeProvider.otherwise({redirectTo: '/'});
     }]);
-
-
-
-//
-// This is the working indicator
-//
-angular.module('JobIndicator', [])
-        .config(function($httpProvider) {
-    $httpProvider.responseInterceptors.push('myHttpInterceptor');
-    var spinnerFunction = function(data, headersGetter) {
-        // todo start the spinner here
-        $('.ok').slideDown('slow');
-        $('.ok span').text('loading')
-        return data;
-    };
-    $httpProvider.defaults.transformRequest.push(spinnerFunction);
-})
-// register the interceptor as a service, intercepts ALL angular ajax http calls
-        .factory('myHttpInterceptor', function($q, $window) {
-    return function(promise) {
-        return promise.then(function(response) {
-            // do something on success
-            // todo hide the spinner
-            $('.ok').slideUp('slow');
-            return response;
-
-        }, function(response) {
-            // do something on error
-            // todo hide the spinner
-            $('.ok').slideUp('slow');
-            return $q.reject(response);
-        });
-    };
-});
