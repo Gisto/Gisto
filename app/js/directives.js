33fd3daf24bb6b5e6a2f4c024491854f296d1202
Maayan Glikser <maayan@glikm.com>
2013-04-06 23:57:28 +0300

    Ace editor now loads correctly with languages and updating the scope

--- app/js/directives.js
+++ app/js/directives.js
@@ -12,14 +12,50 @@ app.directive('appVersion', ['version', function(version) {
     }]);
 
 app.directive('editor', function($timeout) {
-    return function(scope, element, attrs) {
-        $timeout(function() {
-            var lang = attrs.editor;
-            console.log('Lang to highlight: ' + lang);
+    var editorWindow = angular.element('<pre id="editor-{{$index}}">{{file.content}}</pre>');
+    return {
+      restrict:'E',
+      compile: function(elem) {
+        elem.append(editorWindow);
 
-            var editor = ace.edit(element[0]);
-            editor.setTheme("ace/theme/tomorrow");
-            editor.getSession().setMode("ace/mode/" + lang);
-        }, 0);
+        return function(scope, element, attrs) {
+
+            $timeout(function() {
+
+                var lang = attrs.language,
+                    editor = ace.edit('editor-' + attrs.index);
+
+                console.log({language: lang});
+
+                editor.setTheme("ace/theme/tomorrow");
+                editor.getSession().setMode("ace/mode/" + lang);
+
+                editor.on('change', function(data) {
+                    scope.file.content = editor.getValue();
+                });
+
+            }, 0);
+        }
+      }
+
     };
 });
+
+
+//
+//
+//    return function(scope, element, attrs) {
+//        $timeout(function() {
+//            var lang = attrs.editor;
+//            var editor = ace.edit(element[0]);
+//            editor.setTheme("ace/theme/tomorrow");
+//            editor.getSession().setMode("ace/mode/" + lang);
+//
+//            editor.on('change', function(data) {
+//               console.log(editor.getValue());
+//
+//            });
+//
+//        }, 0);
+//    };
+//});
--- app/js/filters.js
+++ app/js/filters.js
@@ -8,9 +8,43 @@ angular.module('myApp.filters', []).
             return String(text).replace(/\%VERSION\%/mg, version);
         }
     }])
-        .filter('publicOrPrivet', function() {
+    .filter('publicOrPrivet', function() {
     return function(input) {
         return input ? 'public' : 'secret';
     };
-});
+    }).filter('codeLanguage', function() {
+
+        var languages = {
+            "C#":"csharp",
+            "C++":"c_cpp",
+            Clojure:"clojure",
+            CoffeeScript:"coffee",
+            ColdFusion:"coldfusion",
+            CSS:"css",
+            Groovy:"groovy",
+            Haxe:"haxe",
+            HTML:"html",
+            Java:"java",
+            JavaScript:"javascript",
+            JSON:"json",
+            Lua:"lua",
+            Markdown:"markdown",
+            OCaml:"ocaml",
+            Perl:"perl",
+            PHP:"php",
+            PowerShell:"powershell",
+            Python:"python",
+            Ruby:"ruby",
+            Scala:"scala",
+            SCSS:"scss",
+            SQL:"sql",
+            TeX:"latex",
+            Textile:"textile",
+            XML:"xml"
+        };
+
+        return function(input) {
+            return languages.hasOwnProperty(input) ? languages[input] : input;
+        };
+    });
 
