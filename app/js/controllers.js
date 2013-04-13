a4de20485d1efd44c5b519e68f6a19fb6ddd1a8d
Sasha Khamkov <sanusart@gmail.com>
2013-04-14 02:56:50 +0300

    ghAPI WIP

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,150 +1,148 @@
 'use strict';
 /* Controllers */
 
-function listGistCtrl($scope, $http) {
-    $http.get('http://localhost:3000/gists')
-            .success(function(data) {
-        for (var item in data) {
-            data[item].tags = data[item].description ? data[item].description.match(/(#[A-Za-z0-9\-\_]+)/g) : [];
-        }
-        $scope.gists = data;
-    });
+function listGistCtrl($scope, $http, db, ghAPI) {
+    /*
+     db.set('gisto_auth', {token: ''}, function(response) {
+     if (response.status === 'ok') {
+     console.log('OK');
+     } else {
+     console.log('NADA!!!');
+     }
+     });
+
+     db.get('gisto_auth', function(response) {
+     console.log(response.data.token);
+     });
+     */
+    var data = ghAPI.gists(); // Get the gists list
+    for (var item in data) {
+        data[item].tags = data[item].description ? data[item].description.match(/(#[A-Za-z0-9\-\_]+)/g) : [];
+    }
+    $scope.gists = data;
 }
 
-function singleGistCtrl($scope, $routeParams, $http, db) {
-    //console.log(db);
-    db.get('myTest5', function(response) {
-       console.log(response);
-    });
-    $http.get('http://localhost:3000/gists/' + $routeParams.gistId)
-            .success(function(data) {
-        $scope.single = data;
-        $scope.tags = data.description ? data.description.match(/(#[A-Za-z0-9\-\_]+)/g) : [];
+function singleGistCtrl($scope, $routeParams, $http, db, ghAPI) {
+    var data = ghAPI.gist($routeParams.gistId);
+    $scope.single = data;
+    $scope.tags = data.description ? data.description.match(/(#[A-Za-z0-9\-\_]+)/g) : [];
 
-        $scope.enableEdit = function() {
-            $scope.edit = true;
-            $('.edit').slideDown('slow');
+    $scope.enableEdit = function() {
+        $scope.edit = true;
+        $('.edit').slideDown('slow');
+    };
+    $scope.disableEdit = function() {
+        $scope.edit = false;
+    };
+
+    $scope.warnDeleteGist = function() {
+        $('.delete').slideDown('slow');
+    };
+    $scope.cancelDeleteGist = function() {
+        $('.delete').slideUp('slow');
+    };
+
+    $scope.del = function($event) {
+        if ($event) {
+            $event.preventDefault();
+        }
+        var response = ghAPI.delete($scope.single.id);
+        if (response === 'ok') {
+            console.log(response);
+            $('.warn').slideDown('slow');
+            $('.warn span').text('Gist deleted');
+            setTimeout(function() {
+                $('.warn').slideUp();
+            }, 2500);
+        }
+    };
+
+    $scope.addFile = function() {
+        var fileName = 'newFile' + Object.keys($scope.single.files).length + '.txt';
+        $scope.single.files[fileName] = {
+            content: '',
+            filename: fileName,
+            language: 'text'
         };
-        $scope.disableEdit = function() {
+    };
+
+    $scope.dragStart = function(e) {
+        e.stopPropagation();
+        e.preventDefault();
+        $('.edit').slideDown('slow');
+        $('.main section').addClass('dragarea');
+        $('.edit span').text('Drag detected - now drop!');
+        console.log('dragging start');
+    };
+
+    $scope.drop = function(e) {
+        e.stopPropagation();
+        e.preventDefault();
+        var data = event.dataTransfer;
+        for (var i = 0; i < data.files.length; i++) { // For each dropped file
+            var file = data.files[i];
+            var reader = new FileReader();
+
+            $('.edit').slideUp('slow');
+            $('.ok').slideDown('slow');
+            $('.main section').removeClass('dragarea');
+            $('.ok span').html('Dropped: <b>' + file.name + '</b>');
+            reader.onloadend = (function(filename) {
+                return function(event) {
+                    $scope.single.files[filename] = {
+                        filename: filename,
+                        content: event.target.result,
+                        language: 'html'
+                    };
+                    $scope.$digest();
+                }
+            })(file.name);
+
+            reader.readAsText(file);
+
+        }
+    };
+
+    $scope.dragEnd = function(e) {
+        e.stopPropagation();
+        e.preventDefault();
+        console.log('drag end');
+    };
+
+
+
+    $scope.save = function($event) {
+        $('.loading span').text('Saving...');
+        $('.edit').slideUp();
+        if ($event) {
+            $event.preventDefault();
+        }
+
+        var data = {
+            description: $scope.single.description,
+            id: $scope.single.id,
+            files: {}
+        };
+
+        for (var file in $scope.single.files) {
+            data.files[file] = {
+                content: $scope.single.files[file].content,
+                filename: $scope.single.files[file].filename
+            };
+        }
+
+        var response = ghAPI.edit(data);
+        if (response.status === 'ok') {
+            $('.ok').slideDown('slow');
+            $('.ok span').text('Gist saved');
             $scope.edit = false;
-        };
+            $scope.single.history = response.data.history;
+            setTimeout(function() {
+                $('.ok').slideUp();
+            }, 2500);
+        }
+    };
 
-        $scope.warnDeleteGist = function() {
-            $('.delete').slideDown('slow');
-        };
-        $scope.cancelDeleteGist = function() {
-            $('.delete').slideUp('slow');
-        };
-
-        $scope.del = function($event) {
-            if ($event) {
-                $event.preventDefault();
-            }
-            var data = {
-                id: $scope.single.id
-            };
-            $http.get('http://localhost:3000/gists/delete/' + $scope.single.id)
-                    .success(function(response) {
-                if (response.status === 'ok') {
-                    $('.warn').slideDown('slow');
-                    $('.warn span').text('Gist deleted');
-                    setTimeout(function() {
-                        $('.warn').slideUp();
-                    }, 2500);
-                }
-            });
-        };
-
-        $scope.addFile = function() {
-            var fileName = 'newFile' + Object.keys($scope.single.files).length + '.txt';
-            $scope.single.files[fileName] = {
-                content: '',
-                filename: fileName,
-                language: 'text'
-            };
-        };
-
-        $scope.dragStart = function(e) {
-            e.stopPropagation();
-            e.preventDefault();
-            $('.edit').slideDown('slow');
-            $('.main section').addClass('dragarea');
-            $('.edit span').text('Drag detected - now drop!');
-            console.log('dragging start');
-        };
-
-        $scope.drop = function(e) {
-            e.stopPropagation();
-            e.preventDefault();
-            var data = event.dataTransfer;
-            for (var i = 0; i < data.files.length; i++) { // For each dropped file
-                var file = data.files[i];
-                var reader = new FileReader();
-
-                $('.edit').slideUp('slow');
-                $('.ok').slideDown('slow');
-                $('.main section').removeClass('dragarea');
-                $('.ok span').html('Dropped: <b>' + file.name + '</b>');
-                reader.onloadend = (function(filename) {
-                    return function(event) {
-                        $scope.single.files[filename] = {
-                            filename: filename,
-                            content: event.target.result,
-                            language: 'html'
-                        };
-                        $scope.$digest();
-                    }
-                })(file.name);
-
-                reader.readAsText(file);
-
-            }
-        };
-
-        $scope.dragEnd = function(e) {
-            e.stopPropagation();
-            e.preventDefault();
-            console.log('drag end');
-        };
-
-
-
-        $scope.save = function($event) {
-            $('.loading span').text('Saving...');
-            $('.edit').slideUp();
-            if ($event) {
-                $event.preventDefault();
-            }
-
-            var data = {
-                description: $scope.single.description,
-                id: $scope.single.id,
-                files: {}
-            };
-
-            for (var file in $scope.single.files) {
-                data.files[file] = {
-                    content: $scope.single.files[file].content,
-                    filename: $scope.single.files[file].filename
-                };
-            }
-
-            $http.post('http://localhost:3000/gists/edit', data)
-                    .success(function(response) {
-                if (response.status === 'ok') {
-                    $('.ok').slideDown('slow');
-                    $('.ok span').text('Gist saved');
-                    $scope.edit = false;
-                    $scope.single.history = response.data.history;
-                    setTimeout(function() {
-                        $('.ok').slideUp();
-                    }, 2500);
-                }
-            });
-
-        };
-
-    });
 }
 
 function commentsGistCtrl($scope, $routeParams, $http) {
@@ -154,7 +152,7 @@ function commentsGistCtrl($scope, $routeParams, $http) {
     });
 }
 
-function createGistCtrl($scope, $routeParams, $http) {
+function createGistCtrl($scope, $routeParams, $http, ghAPI) {
     $scope.description = '';
     $scope.isPublic = false;
     $scope.files = [
@@ -192,13 +190,11 @@ function createGistCtrl($scope, $routeParams, $http) {
             };
         }
 
-        $http.post('http://localhost:3000/gists/create', data)
-                .success(function(response) {
-            if (response.status === 'ok') {
-                $('.ok').slideDown('slow');
-                $('.ok span').text('Gist saved');
-                window.location.href = "#/gist/" + response.data.id
-            }
-        });
+        var response = ghAPI.create(data);
+        if (response === 'OK') {
+            $('.ok').slideDown('slow');
+            $('.ok span').text('Gist saved');
+            window.location.href = "#/gist/" + response.data.id;
+        }
     }
 }
