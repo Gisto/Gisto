f5c46c9f9a554cfd5db07725d536a24193172f97
Sasha Khamkov <sanusart@gmail.com>
2013-04-18 01:28:14 +0300

    Fixed redirect after login. Added link to download whole gist gist to toolbar and download file on each file.

--- app/js/services.js
+++ app/js/services.js
@@ -5,53 +5,53 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
-        value('version', '0.1');
+    value('version', '0.1');
 //
 // This is the working indicator
 //
 angular.module('JobIndicator', [])
-        .config(function($httpProvider) {
-    $httpProvider.responseInterceptors.push('myHttpInterceptor');
-    var spinnerFunction = function(data, headersGetter) {
+    .config(function ($httpProvider) {
+        $httpProvider.responseInterceptors.push('myHttpInterceptor');
+        var spinnerFunction = function (data, headersGetter) {
 // todo start the spinner here
-        $('.loading').slideDown('slow');
-        return data;
-    };
-    $httpProvider.defaults.transformRequest.push(spinnerFunction);
-})
+            $('.loading').slideDown('slow');
+            return data;
+        };
+        $httpProvider.defaults.transformRequest.push(spinnerFunction);
+    })
 // register the interceptor as a service, intercepts ALL angular ajax http calls
-        .factory('myHttpInterceptor', function($q, $window) {
-    return function(promise) {
-        return promise.then(function(response) {
+    .factory('myHttpInterceptor', function ($q, $window) {
+        return function (promise) {
+            return promise.then(function (response) {
 // do something on success
 // todo hide the spinner
-            $('.loading').slideUp('slow');
-            return response;
-        }, function(response) {
+                $('.loading').slideUp('slow');
+                return response;
+            }, function (response) {
 // do something on error
 // todo hide the spinner
 //$('.warn').slideDown('slow');
 //$('.warn span').text('Something not right');
-            console.info('services.js -> "JobIndicator" condition went to error.');
-            return $q.reject(response);
-        });
-    };
-});
-var ghAPI = angular.module('gitHubAPI', [], function($provide) {
-    $provide.factory('ghAPI', function($http) {
+                console.info('services.js -> "JobIndicator" condition went to error.');
+                return $q.reject(response);
+            });
+        };
+    });
+var ghAPI = angular.module('gitHubAPI', [], function ($provide) {
+    $provide.factory('ghAPI', function ($http) {
         var api_url = 'https://api.github.com/gists',
-                token = localStorage.token;
+            token = localStorage.token;
         var api = {
 // POST /authorizations
-            login: function(user, pass, callback) {
+            login: function (user, pass, callback) {
                 $http({
                     method: 'POST',
                     url: 'https://api.github.com/authorizations',
                     data: {"scopes": [
-                            "repo",
-                            "gist",
-                            "user"
-                        ],
+                        "repo",
+                        "gist",
+                        "user"
+                    ],
                         "note": "Gisto"
                     },
                     headers: {
@@ -59,7 +59,7 @@ var ghAPI = angular.module('gitHubAPI', [], function($provide) {
                         "Authorization": "Basic " + btoa(user + ":" + pass),
                         "Content-Type": "application/x-www-form-urlencoded"
                     }
-                }).success(function(data, status, headers, config) {
+                }).success(function (data, status, headers, config) {
 //                    console.log('>>> DATA');
 //                    console.log(data);
 //                    console.log('>>> STATUS');
@@ -68,27 +68,27 @@ var ghAPI = angular.module('gitHubAPI', [], function($provide) {
 //                    console.log(headers());
 //                    console.log('>>> CONFIG');
 //                    console.log(config);
-                    return callback({
-                        data: data,
-                        status: status,
-                        headers: headers(),
-                        config: config
-                    });
-                }).error(function(data, status, headers, config) {
+                        return callback({
+                            data: data,
+                            status: status,
+                            headers: headers(),
+                            config: config
+                        });
+                    }).error(function (data, status, headers, config) {
 //                    console.log(data);
 //                    console.log(status);
 //                    console.log(headers());
 //                    console.log(config);
-                    return callback({
-                        data: data,
-                        status: status,
-                        headers: headers(),
-                        config: config
+                        return callback({
+                            data: data,
+                            status: status,
+                            headers: headers(),
+                            config: config
+                        });
                     });
-                });
             },
             // GET /gists
-            gists: function(callback, updateOnly, pageNumber) {
+            gists: function (callback, updateOnly, pageNumber) {
                 var url = pageNumber ? api_url + '?page=' + pageNumber : api_url,
                     headers = {
                         Authorization: 'token ' + token
@@ -102,67 +102,67 @@ var ghAPI = angular.module('gitHubAPI', [], function($provide) {
                     method: 'GET',
                     url: url,
                     headers: headers
-                }).success(function(data, status, headers, config) {
-                    var data = {
-                        data: data,
-                        status: status,
-                        headers: headers(),
-                        config: config
-                    };
-                    localStorage.gistsLastUpdated = data.headers['last-modified'];
-                    callback(data);
+                }).success(function (data, status, headers, config) {
+                        var data = {
+                            data: data,
+                            status: status,
+                            headers: headers(),
+                            config: config
+                        };
+                        localStorage.gistsLastUpdated = data.headers['last-modified'];
+                        callback(data);
 
-                  var links = data.headers.link.split(',');
-                  for (var link in links) {
-                      link = links[link];
-                      if (link.indexOf('rel="next') > -1) {
-                          pageNumber = link.match(/[0-9]+/)[0];
-                          api.gists(callback, null, pageNumber);
-                      }
-                  }
-                }).error(function(data, status, headers, config) {
+                        var links = data.headers.link.split(',');
+                        for (var link in links) {
+                            link = links[link];
+                            if (link.indexOf('rel="next') > -1) {
+                                pageNumber = link.match(/[0-9]+/)[0];
+                                api.gists(callback, null, pageNumber);
+                            }
+                        }
+                    }).error(function (data, status, headers, config) {
 //                    console.log(data);
 //                    console.log(status);
 //                    console.log(headers());
 //                    console.log(config);
-                    return callback({
-                        data: data,
-                        status: status,
-                        headers: headers(),
-                        config: config
+                        return callback({
+                            data: data,
+                            status: status,
+                            headers: headers(),
+                            config: config
+                        });
                     });
-                });
             },
             // GET /gists/:id
-            gist: function(id, callback) {
+            gist: function (id, callback) {
                 $http({
                     method: 'GET',
                     url: api_url + '/' + id,
                     headers: {
                         Authorization: 'token ' + token
                     }
-                }).success(function(data, status, headers, config) {
+                }).success(function (data, status, headers, config) {
 //                    console.log(data);
 //                    console.log(status);
 //                    console.log(headers());
 //                    console.log(config);
-                    return callback({
-                        data: data,
-                        status: status,
-                        headers: headers(),
-                        config: config
+                        return callback({
+                            data: data,
+                            status: status,
+                            headers: headers(),
+                            config: config
+                        });
+                    }).error(function (data, status, headers, config) {
+                        return callback({
+                            data: data,
+                            status: status,
+                            headers: headers(),
+                            config: config
+                        });
                     });
-                }).error(function(data, status, headers, config) {
-                    return callback({
-                        data: data,
-                        status: status,
-                        headers: headers(),
-                        config: config
-                    });
-                });
             },
             // POST /gists
-            create: function(data, callback) {
+            create: function (data, callback) {
                 $http({
                     method: 'POST',
                     url: api_url,
@@ -170,28 +170,28 @@ var ghAPI = angular.module('gitHubAPI', [], function($provide) {
                     headers: {
                         Authorization: 'token ' + token
                     }
-                }).success(function(data, status, headers, config) {
+                }).success(function (data, status, headers, config) {
 //                    console.log(data);
 //                    console.log(status);
 //                    console.log(headers());
 //                    console.log(config);
-                    return callback({
-                        data: data,
-                        status: status,
-                        headers: headers(),
-                        config: config
+                        return callback({
+                            data: data,
+                            status: status,
+                            headers: headers(),
+                            config: config
+                        });
+                    }).error(function (data, status, headers, config) {
+                        return callback({
+                            data: data,
+                            status: status,
+                            headers: headers(),
+                            config: config
+                        });
                     });
-                }).error(function(data, status, headers, config) {
-                    return callback({
-                        data: data,
-                        status: status,
-                        headers: headers(),
-                        config: config
-                    });
-                });
             },
             // PATCH /gists/:id
-            edit: function(id, data, callback) {
+            edit: function (id, data, callback) {
                 $http({
                     method: 'PATCH',
                     url: api_url + '/' + id,
@@ -199,96 +199,96 @@ var ghAPI = angular.module('gitHubAPI', [], function($provide) {
                     headers: {
                         Authorization: 'token ' + token
                     }
-                }).success(function(data, status, headers, config) {
+                }).success(function (data, status, headers, config) {
 //                    console.log(data);
 //                    console.log(status);
 //                    console.log(headers());
 //                    console.log(config);
-                    return callback({
-                        data: data,
-                        status: status,
-                        headers: headers(),
-                        config: config
+                        return callback({
+                            data: data,
+                            status: status,
+                            headers: headers(),
+                            config: config
+                        });
+                    }).error(function (data, status, headers, config) {
+                        return callback({
+                            data: data,
+                            status: status,
+                            headers: headers(),
+                            config: config
+                        });
                     });
-                }).error(function(data, status, headers, config) {
-                    return callback({
-                        data: data,
-                        status: status,
-                        headers: headers(),
-                        config: config
-                    });
-                });
             },
             // DELETE /gists/:id
-            delete: function(id, callback) {
+            delete: function (id, callback) {
                 $http({
                     method: 'DELETE',
                     url: api_url + '/' + id,
                     headers: {
                         Authorization: 'token ' + token
                     }
-                }).success(function(data, status, headers, config) {
+                }).success(function (data, status, headers, config) {
 //                    console.log(data);
 //                    console.log(status);
 //                    console.log(headers());
 //                    console.log(config);
-                    return callback({
-                        data: data,
-                        status: status,
-                        headers: headers(),
-                        config: config
+                        return callback({
+                            data: data,
+                            status: status,
+                            headers: headers(),
+                            config: config
+                        });
+                    }).error(function (data, status, headers, config) {
+                        return callback({
+                            data: data,
+                            status: status,
+                            headers: headers(),
+                            config: config
+                        });
                     });
-                }).error(function(data, status, headers, config) {
-                    return callback({
-                        data: data,
-                        status: status,
-                        headers: headers(),
-                        config: config
-                    });
-                });
             },
             // GET /gists/:id/comments
-            comments: function(id, callback) {
+            comments: function (id, callback) {
                 $http({
                     method: 'GET',
                     url: api_url + '/' + id + '/comments',
                     headers: {
                         Authorization: 'token ' + token
                     }
-                }).success(function(data, status, headers, config) {
+                }).success(function (data, status, headers, config) {
 //                    console.log(data);
 //                    console.log(status);
 //                    console.log(headers());
 //                    console.log(config);
-                    return callback({
-                        data: data,
-                        status: status,
-                        headers: headers(),
-                        config: config
+                        return callback({
+                            data: data,
+                            status: status,
+                            headers: headers(),
+                            config: config
+                        });
+                    }).error(function (data, status, headers, config) {
+                        return callback({
+                            data: data,
+                            status: status,
+                            headers: headers(),
+                            config: config
+                        });
                     });
-                }).error(function(data, status, headers, config) {
-                    return callback({
-                        data: data,
-                        status: status,
-                        headers: headers(),
-                        config: config
-                    });
-                });
             },
             // GET /gists/starred
-            starred: function() {
+            starred: function () {
             },
             // PUT /gists/:id/star
-            star: function() {
+            star: function () {
             },
             // DELETE /gists/:id/star
-            unstar: function() {
+            unstar: function () {
             },
             // GET /gists/:id/star
-            is_starred: function() {
+            is_starred: function () {
             },
             // POST /gists/:id/forks
-            fork: function() {
+            fork: function () {
             }
         };
 
