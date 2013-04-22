be1c72d2267b19ea57a39d43332bc12cc641e466
Sasha Khamkov <sanusart@gmail.com>
2013-04-22 11:15:49 +0300

    Settings works. Rewriting settings as service. WIP

--- app/js/app.js
+++ app/js/app.js
@@ -1,14 +1,9 @@
 'use strict';
 
-
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['ui', 'myApp.filters', 'myApp.services', 'myApp.directives', 'JobIndicator', 'gitHubAPI', 'gistData']).
     config(['$routeProvider', function ($routeProvider) {
-        if (localStorage.token) {
-            $routeProvider.when('/', {templateUrl: 'partials/empty.html'});
-        } else {
-            $routeProvider.when('/', {templateUrl: 'login.html', controller: loginCtrl});
-        }
+        $routeProvider.when('/', {templateUrl: 'partials/empty.html'});
         $routeProvider.when('/login', {templateUrl: 'login.html', controller: loginCtrl});
         $routeProvider.when('/settings', {templateUrl: 'partials/settings.html'/*, controller: loginCtrl*/});
         $routeProvider.when('/gist/:gistId', {templateUrl: 'partials/single-gist.html', controller: singleGistCtrl});
