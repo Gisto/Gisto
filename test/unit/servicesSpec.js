6bea5e6de800e6c7571cebb936640f085d5a4e16
Sasha Khamkov <sanusart@gmail.com>
2013-05-29 22:11:50 +0300

    Changed main app namespace from "myApp" to "gisto"

--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -3,7 +3,7 @@
 /* jasmine specs for services go here */
 
 describe('service', function() {
-  beforeEach(module('myApp.services'));
+  beforeEach(module('gisto.services'));
 
 
   describe('version', function() {
