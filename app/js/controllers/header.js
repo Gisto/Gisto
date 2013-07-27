69f10c080e46bd704e16d0a5bfa62a068977659e
Maayan Glikser <maayan@glikm.com>
2013-07-28 00:41:13 +0300

    Added notification count and edited the headers to use it.

--- app/js/controllers/header.js
+++ app/js/controllers/header.js
@@ -3,6 +3,7 @@
 function headerController($scope, $rootScope, notificationService) {
 
     $scope.avatar = 'https://secure.gravatar.com/avatar/' + JSON.parse(localStorage.settings).avatar;
+    $scope.notificationCount = 0;
 
     if (!$rootScope.hasOwnProperty('userRegistered') || !$rootScope.userRegistered)  {
         notificationService.register();
