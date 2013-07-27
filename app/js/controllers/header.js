b3bf8770ae83ea4d9dcfc3222fc179a40f9319f7
Maayan Glikser <maayan@glikm.com>
2013-07-28 00:32:47 +0300

    Added notifications header to all views, sharing gist button WIP

--- app/js/controllers/header.js
+++ app/js/controllers/header.js
@@ -1,5 +1,12 @@
 'use strict';
 
-function headerController($scope) {
+function headerController($scope, $rootScope, notificationService) {
+
+    $scope.avatar = 'https://secure.gravatar.com/avatar/' + JSON.parse(localStorage.settings).avatar;
+
+    if (!$rootScope.hasOwnProperty('userRegistered') || !$rootScope.userRegistered)  {
+        notificationService.register();
+    }
+
 
 }
