5706ef2eb877fbba68e33213a44f8f333c4aa37e
Sasha Khamkov <sanusart@gmail.com>
2013-08-01 22:28:45 +0300

    Fix: media query of date_meta, Fix: editors now have height based on lines of code.instead of fixed 400px.

--- app/js/controllers/commentsController.js
+++ app/js/controllers/commentsController.js
@@ -1,6 +1,7 @@
 'use strict';
 
 function commentsGistCtrl($scope, $routeParams, ghAPI) {
+    console.log('>>>>>>>>>>>>> Comments',$scope.gist.comments);
     if ($scope.gist.comments > 0) {
         ghAPI.comments($routeParams.gistId, function (response) {
             if (response.status === 200) {
