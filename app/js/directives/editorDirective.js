e3e32d05c7df2ae51c88e49d9d056f1b6748c53f
Sasha Khamkov <sanusart@gmail.com>
2013-08-01 22:28:45 +0300

    Fix: media query of date_meta, Fix: editors now have height based on lines of code.instead of fixed 400px.

--- app/js/directives/editorDirective.js
+++ app/js/directives/editorDirective.js
@@ -23,12 +23,24 @@ angular.module('gisto.directive.editor', []).directive('editor', function ($time
                     editor.setShowPrintMargin(false);
                     editor.setAutoScrollEditorIntoView(true);
 
+                    function fitSize() {
+                    var newHeight =
+                        editor.getSession().getScreenLength()
+                            * editor.renderer.lineHeight
+                            + editor.renderer.scrollBar.getWidth();
+
+                    $('pre#editor-' + attrs.index).height(newHeight.toString() + "px");
+                    editor.resize(true);
+                    }
+
+                    fitSize();
+
                     console.log('language:', lang);
                     console.log('Theme:', theme);
                     console.log('Font size:', font);
 
                     editor.on('change', function (data) {
-
+                        fitSize();
                         scope.$apply(function () {
                             scope.file.content = editor.getValue();
                             if (!scope.edit) {
