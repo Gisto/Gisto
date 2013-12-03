672e4820885cc9b7946075fd1de3f428ab4f1a0c
Sasha Khamkov <sanusart@gmail.com>
2013-12-04 00:23:16 +0200

    Merged /server/ from Mayyan's branch.

--- server/app.js
+++ server/app.js
@@ -1,14 +1,15 @@
-var app = require('express')()
-    , server = require('http').createServer(app)
-    , io = require('socket.io').listen(server)
-    , clientToken = require('./config.js').clientToken
-    , analytics = require('nodealytics');
+
+var app = require('express')(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server),
+    clientToken = require('./config.js').clientToken,
+    analytics = require('nodealytics');
 
 var databaseUrl = "gisto",
     collections = ["notifications"],
     db = require('mongojs').connect(databaseUrl, collections);
 
-analytics.initialize('UA-40972813-1', 'gistoapp.com', function () {
+analytics.initialize('UA-40972813-1', 'gistoapp.com', function() {
     //MORE GOOGLE ANALYTICS CODE HERE
 });
 
@@ -16,14 +17,14 @@ server.listen(3000);
 
 var clients = [];
 
-io.sockets.on('connection', function (client) {
+io.sockets.on('connection', function(client) {
 
-   console.log('client connected');
+    console.log('client connected');
     io.sockets.socket(client.id).emit('identify');
 
-    client.on('registerClient', function (data) {
+    client.on('registerClient', function(data) {
         console.log(data);
-        if ( !data.hasOwnProperty('token') || data.token !== clientToken) {
+        if (!data.hasOwnProperty('token') || data.token !== clientToken) {
             console.log('failed authentication');
             client.disconnect();
             return;
@@ -34,22 +35,29 @@ io.sockets.on('connection', function (client) {
         clients.push(client);
 
         // check for existing notifications
-        db.notifications.find({recipient: data.user}, function(err, notifications) {
-           if (err || !notifications) {
-               console.log('no pending notifications');
-           }  else {
+        db.notifications.find({
+            recipient: data.user
+        }, function(err, notifications) {
+            if (err || !notifications) {
+                console.log('no pending notifications');
+            } else {
 
-               notifications.forEach( function(notification) {
-                   io.sockets.socket(client.id).emit('receiveNotification', notification);
-               } );
-           }
+                notifications.forEach(function(notification) {
+                    io.sockets.socket(client.id).emit('receiveNotification', notification);
+                });
+            }
         });
 
-        analytics.trackEvent('clientLogin', data.user, function (err, resp) {
-           if (!err && resp.statusCode === 200) {
-               console.log('Event has been tracked');
-           }
-        });
+        var userAgent = data['useragent'] || 'application';
+
+        if (userAgent !== 'plugin') {
+            analytics.trackEvent('clientLogin', data.user, function(err, resp) {
+                if (!err && resp.statusCode === 200) {
+                    console.log('Event has been tracked');
+                }
+            });
+            console.log('track login');
+        }
 
 
     });
@@ -64,16 +72,21 @@ io.sockets.on('connection', function (client) {
         console.log('recieved notification');
 
         // remove notification from database
-        db.notifications.remove({recipient: client.user, gistId: item.gistId}, false);
+        db.notifications.remove({
+            recipient: client.user,
+            gistId: item.gistId
+        }, false);
 
         // send all clients that the notification has been read.
-        var recipient = getAllClientSockets(clients,client.user);
+        var recipient = getAllClientSockets(clients, client.user);
 
         if (recipient && recipient.length > 0) {
 
             for (var i = 0, limit = recipient.length; i < limit; i++) {
                 console.log('sending notification: ' + i);
-                io.sockets.socket(recipient[i].id).emit('notificationRead',  {gistId: item.gistId});
+                io.sockets.socket(recipient[i].id).emit('notificationRead', {
+                    gistId: item.gistId
+                });
             }
         }
 
@@ -81,35 +94,31 @@ io.sockets.on('connection', function (client) {
 
     client.on('sendNotification', function(data) {
 
-        var recipient = getAllClientSockets(clients,data.recipient);
+        var recipient = getAllClientSockets(clients, data.recipient);
         console.log('clients', recipient);
 
-        // construct notification
-        var notification = {
-
-        };
+        // add the sender
+        data.type = data.type || 'share';
+        if (data.type === 'share') {
+            data.sender = client.user;
+        }
 
         if (recipient && recipient.length > 0) {
 
             for (var i = 0, limit = recipient.length; i < limit; i++) {
-                io.sockets.socket(recipient[i].id).emit('receiveNotification', { sender: client.user, gistId: data.gistId, name: data.name, gravatar_id: data.gravatar_id});
+                io.sockets.socket(recipient[i].id).emit('receiveNotification', data);
             }
         }
 
         // save the notification
-        db.notifications.save({
-            sender: client.user,
-            recipient: data.recipient,
-            gistId: data.gistId,
-            name: data.name,
-            gravatar_id: data.gravatar_id
-        }, function(err, saved) {
-           if (err || !saved) {
-               console.log('notification failed to save');
-           }  else {
-               console.log('notification saved');
-           }
+        db.notifications.save(data, function(err, saved) {
+            if (err || !saved) {
+                console.log('notification failed to save');
+            } else {
+                console.log('notification saved');
+            }
         });
+
 
     });
 });
@@ -118,4 +127,4 @@ function getAllClientSockets(clients, username) {
     return clients.filter(function(item) {
         return item.user === username;
     });
-}
+} 
