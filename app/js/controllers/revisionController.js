9fff85de6d6274371fabe84ff4d773631f885f99
Sasha Khamkov <sanusart@gmail.com>
2013-12-10 20:35:02 +0200

    * Fix outside links to work via custom directive.
    * Small code and UI fixes in oartials.
    
    * Minified with grunt as "Production".

--- app/js/controllers/revisionController.js
+++ app/js/controllers/revisionController.js
@@ -5,4 +5,25 @@ function singleGistHistoryCtrl($scope, $routeParams, gistData, ghAPI) {
     $scope.version_hash = $routeParams.gistRevisionId;
 
     ghAPI.history($routeParams.gistId,$routeParams.gistRevisionId);
+
+    $scope.copyToClipboard = function (data, message,type) {
+        message = message || 'Content of a file <b>' + data.filename + '</b> copied to clipboard';
+        if (clipboard !== undefined) {
+            if(type === 'embed') {
+                clipboard.set('<script src="' + data + '"></script>');
+            } else {
+                clipboard.set(data.content || data, 'text');
+            }
+        } else {
+            // Copy to clipboard really only works in App
+            console.warn('>>> DEBUG MODE ON | Copy to clipboard really only works in App \n Data: ' + (data.content || data));
+            console.log('data:', data);
+        }
+
+        $('.ok').slideDown('slow');
+        $('.ok span').html(message);
+        setTimeout(function () {
+            $('.ok').slideUp();
+        }, 2500);
+    };
 }
