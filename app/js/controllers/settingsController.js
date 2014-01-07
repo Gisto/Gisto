003ca2cd28c4b27b0d53e48e3f9030f99c1c48bc
Maayan Glikser <maayan@operad.com>
2014-01-07 19:50:37 +0200

    Fixed a bug where saving settings would reset the token and log the user out. Added a promise to the settingsController to load only when localStorage is ready and settings has been loaded.

--- app/js/controllers/settingsController.js
+++ app/js/controllers/settingsController.js
@@ -2,16 +2,20 @@
 
 function settingsCtrl($scope, appSettings,$rootScope) {
     $scope.themes = appSettings.theme_list;
-    $scope.editor_theme = appSettings.get('editor_theme') || 'tomorrow';
     $scope.editor_themes = appSettings.editor_theme_list;
     $scope.font_sizes = appSettings.font_size;
-    $scope.font_size = appSettings.get('font_size') || '13';
-    $scope.min_height = appSettings.get('min_height') || '';
-    $scope.max_height = appSettings.get('max_height') || '';
-    $scope.theme = appSettings.get('theme') || 'default';
-    $scope.anim = appSettings.get('anim') || 1;
-    $scope.token = appSettings.get('token') || '';
-    $scope.avatar = appSettings.get('avatar') || '';
+
+    appSettings.loadSettings().then(function(result) {
+        $scope.editor_theme = result['editor_theme'] || 'tomorrow';
+        $scope.font_size = result['font_size'] || '13';
+        $scope.min_height = result['min_height'] || '';
+        $scope.max_height = result['max_height'] || '';
+        $scope.theme = result['theme'] || 'default';
+        $scope.anim = result['anim'] || 1;
+    }, function(error) {
+        console.log('could not load settings');
+    });
+
     angular.element('.the-gist pre').css({
         'min-height': $scope.min_height + 'px',
         'max-height': $scope.max_height + 'px'
@@ -24,8 +28,6 @@ function settingsCtrl($scope, appSettings,$rootScope) {
         data.font_size = $scope.font_size;
         data.min_height = $scope.min_height;
         data.max_height = $scope.max_height;
-        data.token = appSettings.get('token');
-        data.avatar = appSettings.get('avatar');
         var saved = appSettings.set(data, function (response) {
             if (response.status === 'ok') {
                 console.log('SAVED SETTINGS');
