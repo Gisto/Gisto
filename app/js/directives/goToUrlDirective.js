6e247337171a59398819ce314651b5518a9e3557
Sasha Khamkov <sanusart@gmail.com>
2014-02-13 23:49:43 +0200

    Fix open links in system browser.

--- app/js/directives/goToUrlDirective.js
+++ app/js/directives/goToUrlDirective.js
@@ -6,8 +6,9 @@ angular.module('gisto.directive.toUrl', []).directive('toUrl', function ($rootSc
         link: function (scope, element, attrs) {
             element.on('click', function (event) {
                if($rootScope.mobile) {
-                    console.log('--------- IN BROWSER URL:', attrs.toUrl.toLowerCase());
-                    navigator.app.loadUrl(attr.toUrl.toLowerCase(), { openExternal:true });
+                    //console.log('--------- IN BROWSER URL:', attrs.toUrl.toLowerCase());
+                    //window.open(attrs.toUrl.toLowerCase(),'_system');
+                    navigator.app.loadUrl(attrs.toUrl.toLowerCase(), { openExternal:true });
                 } else {
                     console.log('--------- EXTERNAL URL:', attrs.toUrl.toLowerCase());
                     gui.Shell.openExternal(attrs.toUrl.toLowerCase());
