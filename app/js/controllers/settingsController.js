f1f363900f2b6b31d006f1aae86c18008ba05006
Sasha Khamkov <sanusart@gmail.com>
2014-02-28 01:08:48 +0200

    Add range selector to controll application size in settings. Fixes #69

--- app/js/controllers/settingsController.js
+++ app/js/controllers/settingsController.js
@@ -1,11 +1,11 @@
 'use strict';
 
-function settingsCtrl($scope, appSettings,$rootScope) {
+function settingsCtrl($scope, appSettings, $rootScope) {
     $scope.themes = appSettings.theme_list;
     $scope.editor_themes = appSettings.editor_theme_list;
     $scope.font_sizes = appSettings.font_size;
 
-    appSettings.loadSettings().then(function(result) {
+    appSettings.loadSettings().then(function (result) {
         $scope.editor_theme = result['editor_theme'] || 'tomorrow';
         $scope.font_size = result['font_size'] || '13';
         $scope.min_height = result['min_height'] || '';
@@ -13,9 +13,18 @@ function settingsCtrl($scope, appSettings,$rootScope) {
         $scope.theme = result['theme'] || 'default';
         $scope.anim = result['anim'] || 1;
         $scope.editor_ext = result['editor_ext'] || {};
-    }, function(error) {
+        $scope.ui_zoom = result['ui_zoom'] || 100;
+    }, function (error) {
         console.log('could not load settings');
     });
+
+    $scope.body_zoom = function () {
+        return{
+            zoom: $scope.ui_zoom + '%'
+        };
+        console.log('body zoom', $scope.ui_zoom);
+    };
+
 
     angular.element('.the-gist pre').css({
         'min-height': $scope.min_height + 'px',
@@ -31,6 +40,7 @@ function settingsCtrl($scope, appSettings,$rootScope) {
         }
         data.editor_theme = $scope.editor_theme;
         data.font_size = $scope.font_size;
+        data.ui_zoom = $scope.ui_zoom;
         data.min_height = $scope.min_height;
         data.max_height = $scope.max_height;
         var saved = appSettings.set(data, function (response) {
