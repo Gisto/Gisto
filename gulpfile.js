918e8109e4f60d438e34867c7d88ecafe8dd05f9
Sasha Khamkov <sanusart@gmail.com>
2014-04-18 17:01:33 +0300

    Switched from Grunt to Gulp (see https://github.com/Gisto/Gisto/blob/master/gulpfile.js)

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var pkg_json = require('./package.json');
 var fs = require('fs');
 var argv = require('yargs').argv;
-var concat = require('gulp-concat');
+var concat = require('gulp-concat-sourcemap');
 var strip_log = require('gulp-strip-debug');
 
 /**
@@ -11,7 +12,7 @@ var strip_log = require('gulp-strip-debug');
  * @usage gulp version
  */
 gulp.task('version', function () {
-    console.log('Version', pkg_json.version);
+    gutil.log('Version', gutil.colors.green(pkg_json.version));
 });
 
 /**
@@ -31,18 +32,83 @@ gulp.task('version_bump', function () {
     });
 });
 
-gulp.task('dev', function () {
-    // Do stuff
+/**
+ * @name dev
+ * @description Change Gisto environment to "development"
+ * @usage gulp dev
+ */
+gulp.task('dev', function(){
+    var files = [
+        './app/index.html'
+    ];
+    var option = {
+        env_dev: 'env:dev',
+        env_prod: 'env:prod',
+        blocking_char: '#'
+    };
+    files.forEach(function (file) {
+        var content = fs.readFileSync(file, "utf8")
+            .replace('<!-- ' + option.env_dev + ' --' + option.blocking_char + '>', '<!-- ' + option.env_dev + ' -->')
+            .replace('<!-- ' + option.env_prod + ' -->', '<!-- ' + option.env_prod + ' --' + option.blocking_char + '>')
+            .replace('/* ' + option.env_dev + ' *' + option.blocking_char + '/', '/* ' + option.env_dev + ' */')
+            .replace('/* ' + option.env_prod + ' */', '/* ' + option.env_prod + ' *' + option.blocking_char + '/');
+        fs.writeFileSync(file, content);
+    });
 });
 
+/**
+ * @name prod
+ * @description Change Gisto environment to "production", also concatenates files and remove console logs
+ * @usage gulp prod
+ */
+gulp.task('prod', ['concat'], function () {
+    var files = [
+        './app/index.html'
+    ];
+    var option = {
+        env_dev: 'env:dev',
+        env_prod: 'env:prod',
+        blocking_char: '#'
+    };
+    files.forEach(function (file) {
+        var content = fs.readFileSync(file, "utf8")
+            .replace('<!-- ' + option.env_prod + ' --' + option.blocking_char + '>', '<!-- ' + option.env_prod + ' -->')
+            .replace('<!-- ' + option.env_dev + ' -->', '<!-- ' + option.env_dev + ' --' + option.blocking_char + '>')
+            .replace('/* ' + option.env_prod + ' *' + option.blocking_char + '/', '/* ' + option.env_prod + ' */')
+            .replace('/* ' + option.env_dev + ' */', '/* ' + option.env_dev + ' *' + option.blocking_char + '/');
+        fs.writeFileSync(file, content);
+    });
+});
+
+/**
+ * @name concat
+ * @description concatenates files and remove console logs, also used by other functions here
+ * @usage gulp concat
+ */
 gulp.task('concat', function () {
+    gulp.src([
+        './app/lib/jquery/*.js',
+        './app/lib/angular/*.js',
+        './app/lib/socket-io/*.js',
+        './app/lib/angular-ui/*.js',
+        './app/js/*/*.js',
+        './app/js/app.js',
+        './app/lib/showdown.js'
+    ])
+        .pipe(strip_log())
+        .pipe(concat('gisto.min.js'))
+        .pipe(gulp.dest('./app/js/'));
+});
+
+/**
+ * @name release
+ * @description Will be used for releases
+ * @usage gulp release
+ */
+gulp.task('release', ['concat'], function () {
     // Do stuff
 });
 
-gulp.task('release', function () {
-    // Do stuff
-});
-
-gulp.task('default', function () {
-    // Do default staff
+gulp.task('default', ['version'], function () {
+   gutil.log('"default" task',gutil.colors.green('All run OK'))
 });
