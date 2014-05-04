9743c011642df0268ec224e1e30df6721ab6a777
Sasha Khamkov <sanusart@gmail.com>
2014-05-04 21:25:55 +0300

    FEATURE (editor): add preview of markdown
    - add preview of markdown files (files with .md extension)

--- app/js/directives/editorDirective.js
+++ app/js/directives/editorDirective.js
@@ -1,26 +1,29 @@
 'use strict';
 
-angular.module('gisto.directive.editor', []).directive('editor', ['$timeout','appSettings',function ($timeout,appSettings) {
-    var editorWindow = angular.element('<pre id="editor-{{$index}}">{{file.content}}</pre><div data-ng-if="editor_ext.statusbar" id="statusBar-{{$index}}"><i class="icon-info-sign"></i> </div>');
+angular.module('gisto.directive.editor', []).directive('editor', ['$timeout', 'appSettings', function ($timeout, appSettings) {
     return {
         restrict: 'E',
-        compile: function (elem) {
-            elem.append(editorWindow);
+        template: '<div ng-if="showmd" class="special-editor-markdown" ng-bind-html-unsafe="file.content | markDown"></div>' +
+        '<pre id="editor-{{$index}}">{{file.content}}</pre>' +
+        '<div data-ng-if="editor_ext.statusbar" id="statusBar-{{$index}}"><i class="icon-info-sign"></i> </div>',
+        link: function ($scope, $element, $attrs) {
+            $scope.showmd = false;
+            if ($attrs.language === 'markdown') {
+                $scope.showmd = true;
+            }
 
-            return function (scope, element, attrs) {
-
-                appSettings.loadSettings().then(function(appSettingsResult) {
+            appSettings.loadSettings().then(function (appSettingsResult) {
 
                 $timeout(function () {
 
-                    var lang = attrs.language,
-                        font = +attrs.font,
-                        indexed = attrs.index,
-                        editor = ace.edit('editor-' + attrs.index),
-                        theme = attrs.theme;
+                    var lang = $attrs.language,
+                        font = $attrs.font,
+                        indexed = $attrs.index,
+                        editor = ace.edit('editor-' + $attrs.index),
+                        theme = $attrs.theme;
 
                     // Emmet
-                    if(lang === 'html' && appSettingsResult.editor_ext.emmet) {
+                    if (lang === 'html' && appSettingsResult.editor_ext.emmet) {
                         console.log('Emmet should be loaded');
                         ace.require("ace/ext/emmet");
                         editor.setOptions({
@@ -28,7 +31,7 @@ angular.module('gisto.directive.editor', []).directive('editor', ['$timeout','ap
                         });
                     }
                     // Auto-completion
-                    if(appSettingsResult.editor_ext.emmet) {
+                    if (appSettingsResult.editor_ext.emmet) {
                         console.log('language tools should be loaded');
                         ace.require("ace/ext/language_tools");
                         editor.setOptions({
@@ -37,9 +40,9 @@ angular.module('gisto.directive.editor', []).directive('editor', ['$timeout','ap
                         });
                     }
                     // Status bar
-                    if(appSettingsResult.editor_ext.statusbar) {
+                    if (appSettingsResult.editor_ext.statusbar) {
                         var StatusBar = ace.require('ace/ext/statusbar').StatusBar;
-                        var statusBar = new StatusBar(editor, document.getElementById('statusBar-'+attrs.index));
+                        var statusBar = new StatusBar(editor, document.getElementById('statusBar-' + $attrs.index));
                     }
 
                     editor.setTheme("ace/theme/" + theme);
@@ -49,7 +52,7 @@ angular.module('gisto.directive.editor', []).directive('editor', ['$timeout','ap
                     editor.renderer.setShowGutter(true);
                     editor.setAutoScrollEditorIntoView(true);
                     var maxLinesSettings = appSettingsResult.max_lines.toString();
-                    var minLinesSettings = (appSettingsResult.min_lines !== 0) ? appSettingsResult.min_lines.toString() : editor.session.getLength().toString() ;
+                    var minLinesSettings = (appSettingsResult.min_lines !== 0) ? appSettingsResult.min_lines.toString() : editor.session.getLength().toString();
                     editor.setOption("maxLines", maxLinesSettings);
                     editor.setOption("minLines", minLinesSettings);
                     editor.resize(true);
@@ -57,23 +60,22 @@ angular.module('gisto.directive.editor', []).directive('editor', ['$timeout','ap
                     console.log('language:', lang);
                     console.log('Theme:', theme);
                     console.log('Font size:', font);
-                    console.log('renderer.lineHeight',editor.renderer.lineHeight);
-                    console.log('# of LINES',editor.session.getLength());
-                    console.log('max_lines',appSettingsResult.max_lines);
-                    console.log('min_lines',appSettingsResult.min_lines);
+                    console.log('renderer.lineHeight', editor.renderer.lineHeight);
+                    console.log('# of LINES', editor.session.getLength());
+                    console.log('max_lines', appSettingsResult.max_lines);
+                    console.log('min_lines', appSettingsResult.min_lines);
 
                     editor.on('change', function (data) {
-                        scope.$apply(function () {
-                            scope.file.content = editor.getValue();
-                            if (!scope.edit) {
-                                scope.enableEdit();
+                        $scope.$apply(function () {
+                            $scope.file.content = editor.getValue();
+                            if (!$scope.edit) {
+                                $scope.enableEdit();
                             }
                         });
                     });
 
                 }, 0);
-                });
-            };
+            });
         }
 
     };
