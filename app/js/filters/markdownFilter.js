b6b2cd5cca1177fe804ef32d6d798b9bb992a845
Maayan Glikser <maayan@glikm.com>
2014-06-07 16:45:10 +0300

    FIX (markdown links): All markdown links now open in a new browser window instead of inside the application.
    Fixes #88

--- app/js/filters/markdownFilter.js
+++ app/js/filters/markdownFilter.js
@@ -1,12 +1,15 @@
 'use strict';
 
-angular.module('gisto.filter.markDown', []).filter('markDown', function () {
+angular.module('gisto.filter.markDown', []).filter('markDown', function ($compile, $rootScope) {
     return function (input) {
         if (typeof input === 'undefined') {
             return;
         } else {
             var converter = new Showdown.converter();
             var html = converter.makeHtml(input);
+            html = html.replace(/href="(.*)"/gi, function (match, p1) {
+                return 'onclick="gui.Shell.openExternal(\'' + p1 + '\')"';
+            });
             return html;
         }
     };
