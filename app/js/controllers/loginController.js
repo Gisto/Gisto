47f17b77a4ee04df47458d7d8570e5cad0c1a282
Maayan Glikser <maayan@glikm.com>
2014-11-04 00:51:50 +0200

    FIX (Login): Add missing changes to the last commit e65ac5a

--- app/js/controllers/loginController.js
+++ app/js/controllers/loginController.js
@@ -20,9 +20,14 @@ function loginCtrl($scope, ghAPI, appSettings, $location, $rootScope) {
         $scope.showEnterpriseSetup = true;
     };
 
+    function isEnterpriseAndEmpty() {
+        console.log('isEnterprise', $scope.active_endpoint === 'enterprise' && (!$scope.enterprise.api_url || !$scope.enterprise.client_id || !$scope.enterprise.client_secret));
+        return $scope.active_endpoint === 'enterprise' && (!$scope.enterprise.api_url || !$scope.enterprise.client_id || !$scope.enterprise.client_secret);
+    }
+
     $scope.hideEnterprise = function () {
 
-        if (!$scope.enterprise.api_url || !$scope.enterprise.client_id || !$scope.enterprise.client_secret) {
+        if (isEnterpriseAndEmpty()) {
             $scope.toggleEnterpriseMode();
         }
 
@@ -39,12 +44,12 @@ function loginCtrl($scope, ghAPI, appSettings, $location, $rootScope) {
             }
 
             // check if /api/v3 is missing from the url and add it
-            if ($scope.enterprise.api_url.toLowerCase().indexOf('/api/v3') === -1) {
+            if ($scope.enterprise.api_url && $scope.enterprise.api_url.toLowerCase().indexOf('/api/v3') === -1) {
                 $scope.enterprise.api_url += '/api/v3';
             }
 
             // check for http(s) protocol and add if missing (assuming http is used)
-            if ($scope.enterprise.api_url.search(/^http(s?):\/\//i) === -1) {
+            if ($scope.enterprise.api_url && $scope.enterprise.api_url.search(/^http(s?):\/\//i) === -1) {
                 $scope.enterprise.api_url = 'http://' + $scope.enterprise.api_url;
             }
 
@@ -76,11 +81,8 @@ function loginCtrl($scope, ghAPI, appSettings, $location, $rootScope) {
             active_endpoint: $scope.active_endpoint
         });
 
-        if ($scope.active_endpoint === 'enterprise' &&
-            (!$scope.enterprise.api_url
-            || !$scope.enterprise.client_id
-            || $scope.enterprise.client_secret)) {
 
+        if (isEnterpriseAndEmpty()) {
             $scope.showEnterprise();
         }
     };
