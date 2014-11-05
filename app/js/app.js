ea0aa7409aca9794ff79cc3e854e284983a01821
Maayan Glikser <maayan@glikm.com>
2014-11-06 01:01:55 +0200

    FIX (settings): Add style-loader directive to manage dynamic styles
    Closes #120

--- app/js/app.js
+++ app/js/app.js
@@ -31,6 +31,7 @@ angular.module('gisto', [
         'gisto.directive.toUrl',
         'gisto.directive.disableAnimate',
         'gisto.directive.toggle',
+        'gisto.directive.styleLoader',
         'gisto.service.gistData',
         'gisto.service.requestHandler',
         'gisto.service.gitHubAPI',
