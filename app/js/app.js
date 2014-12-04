5f49fab95b3618684bfe3131586c3b95dda9fcae
Sasha Khamkov <sanusart@gmail.com>
2014-12-04 02:45:59 +0200

    TOOLS(trackjs): add trackjs
    - add trackjs
    
    Closes #117

--- app/js/app.js
+++ app/js/app.js
@@ -2,37 +2,37 @@
 
 // Declare app level module which depends on filters, and services
 var app = angular.module('gisto', [
-        'ngRoute',
-        'ngAnimate',
-        'ngSanitize',
-        'ui.utils',
-        'angulartics',
-        'angulartics.google.analytics',
-        'gisto.filter.removeTags',
-        'gisto.filter.truncate',
-        'gisto.filter.publicOrPrivet',
-        'gisto.filter.removeTags',
-        'gisto.filter.markDown',
-        'gisto.filter.codeLanguage',
-        'gisto.filter.removeTagSymbol',
-        'gisto.filter.matchTags',
-        'gisto.filter.dotToDash',
-        'gisto.filter.githubFileName',
-        'gisto.directive.scrollTo',
-        'gisto.directive.editor',
-        'gisto.directive.toUrl',
-        'gisto.directive.disableAnimate',
-        'gisto.directive.toggle',
-        'gisto.directive.resourceLoader',
-        'gisto.service.gistData',
-        'gisto.service.requestHandler',
-        'gisto.service.gitHubAPI',
-        'gisto.service.appSettings',
-        'btford.socket-io',
-        'gisto.service.notificationService',
-        'gisto.service.onlineStatusService',
-        'gisto.service.githubUrlBuilder'
-    ]).
+    'ngRoute',
+    'ngAnimate',
+    'ngSanitize',
+    'ui.utils',
+    'angulartics',
+    'angulartics.google.analytics',
+    'gisto.filter.removeTags',
+    'gisto.filter.truncate',
+    'gisto.filter.publicOrPrivet',
+    'gisto.filter.removeTags',
+    'gisto.filter.markDown',
+    'gisto.filter.codeLanguage',
+    'gisto.filter.removeTagSymbol',
+    'gisto.filter.matchTags',
+    'gisto.filter.dotToDash',
+    'gisto.filter.githubFileName',
+    'gisto.directive.scrollTo',
+    'gisto.directive.editor',
+    'gisto.directive.toUrl',
+    'gisto.directive.disableAnimate',
+    'gisto.directive.toggle',
+    'gisto.directive.resourceLoader',
+    'gisto.service.gistData',
+    'gisto.service.requestHandler',
+    'gisto.service.gitHubAPI',
+    'gisto.service.appSettings',
+    'btford.socket-io',
+    'gisto.service.notificationService',
+    'gisto.service.onlineStatusService',
+    'gisto.service.githubUrlBuilder'
+]).
     factory('socket', function (socketFactory) {
         //var socket = io.connect('http://localhost:3001');
         var socket = io.connect('http://server.gistoapp.com:3001');
@@ -43,7 +43,15 @@ var app = angular.module('gisto', [
             ioSocket: socket
         });
     }).
-    config(['$routeProvider', function ($routeProvider) {
+    config(['$routeProvider','$provide', function ($routeProvider,$provide) {
+
+        $provide.decorator("$exceptionHandler", ["$delegate", "$window", function ($delegate, $window) {
+            return function (exception, cause) {
+                if ($window.trackJs) {
+                    $window.trackJs.track(exception);
+                }
+            };
+        }]);
 
         $routeProvider.when('/', {
             templateUrl: 'partials/empty.html',
@@ -82,21 +90,21 @@ var app = angular.module('gisto', [
         $routeProvider.otherwise({
             redirectTo: '/'
         });
-    }]).run(function($rootScope, $timeout, $http) {
+    }]).run(function ($rootScope, $timeout, $http) {
         $rootScope.gistoReady = false;
 
-       $rootScope.$on('$routeChangeStart', function() {
-          $rootScope.edit = false;
-       });
+        $rootScope.$on('$routeChangeStart', function () {
+            $rootScope.edit = false;
+        });
 
         // gisto is ready show the window
         // delay the window by a small amount in order to
         // prevent flickering while settings are loaded.
-        $timeout(function() {
+        $timeout(function () {
             window.win.show();
-        },300);
+        }, 300);
 
-        $http.get('./package.json').then(function(response) {
+        $http.get('./package.json').then(function (response) {
             ga('set', 'appVersion', response.data.version);
         });
 
