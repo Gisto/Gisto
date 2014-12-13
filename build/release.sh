da5cee83daead3714fa37ea98419c2d343889d60
Sasha Khamkov <sanusart@gmail.com>
2014-12-13 20:45:48 +0200

    TOOLS(build/release): improve script to handle OSX and Linux releases
     - improve the flow
    
    Issue #80

--- build/release.sh
+++ build/release.sh
@@ -3,50 +3,71 @@
 ### USAGE ###
 # ../release.sh --linux 0.2.6b
 
-if [[ ! -f "TMP" ]]; then
+# Exit on error
+set -e
+
+BUILD_DIR=`pwd`
+WORKING_DIR="${BUILD_DIR}/TMP"
+PROJECT_DIR=`dirname ${BUILD_DIR}`
+
+if [[ ! -f "${WORKING_DIR}" ]]; then
     mkdir -p TMP
 fi
 
 cd TMP
 
-TMP=`pwd`
-
 architechture="x64 ia32"
 
 mklinux () {
     for arch in ${architechture[@]}; do
-        cp -r ../resources/linux/Gisto-X.X.X-Linux . && mv Gisto-X.X.X-Linux Gisto-${1}-linux_${arch}
-        cp -r ../script/TMP/linux-${arch}/latest-git/* Gisto-${1}-linux_${arch}/gisto
-        tar czf Gisto-${1}-linux_${arch}.tar.gz Gisto-${1}-linux_${arch}
+        cp -r ${BUILD_DIR}/resources/linux/Gisto-X.X.X-Linux ${WORKING_DIR} && mv ${WORKING_DIR}/Gisto-X.X.X-Linux ${WORKING_DIR}/Gisto-${1}-linux_${arch}
+        cp -r ${BUILD_DIR}/script/TMP/linux-${arch}/latest-git/* ${WORKING_DIR}/Gisto-${1}-linux_${arch}/gisto
+        tar -C ${WORKING_DIR} -czf ${WORKING_DIR}/Gisto-${1}-linux_${arch}.tar.gz Gisto-${1}-linux_${arch}
         printf "\nDone Linux ${arch}\n"
     done
     # clean
-    rm -rf Gisto-${1}-linux_{x64,ia32}
+    rm -rf ${WORKING_DIR}/Gisto-${1}-linux_{x64,ia32}
 }
 
 mkosx () {
     for arch in ${architechture[@]}; do
-        if [[ ! -f "libdmg-hfsplus" ]]; then
-            git clone --depth 1 https://github.com/erwint/libdmg-hfsplus
-            cd libdmg-hfsplus && cmake . && make
+        if [[ ! -d "${WORKING_DIR}/libdmg-hfsplus" ]]; then
+            git clone --depth 1 https://github.com/erwint/libdmg-hfsplus ${WORKING_DIR}/libdmg-hfsplus
+            cd ${WORKING_DIR}/libdmg-hfsplus && cmake . && make
         fi
-        cd ${TMP}
-        genisoimage -D -V "Gisto ${1}" -no-pad -r -apple -o gisto-${1}-${arch}-uncompressed.dmg ../script/TMP/osx-${arch}/latest-git/gisto.app
-        ${TMP}/libdmg-hfsplus/dmg/dmg dmg gisto-${1}-${arch}-uncompressed.dmg gisto-${1}-OSX-${arch}.dmg
+        cd ${BUILD_DIR}
+        genisoimage -D -V "Gisto ${1}" -no-pad -r -apple -o ${WORKING_DIR}/gisto-${1}-${arch}-uncompressed.dmg ${BUILD_DIR}/script/TMP/osx-${arch}/latest-git
+        ${WORKING_DIR}/libdmg-hfsplus/dmg/dmg dmg ${WORKING_DIR}/gisto-${1}-${arch}-uncompressed.dmg ${WORKING_DIR}/gisto-${1}-OSX-${arch}.dmg
         printf "\nDone OSX ${arch}\n"
     done
-    rm -rf libdmg-hfsplus gisto-${1}-{x64,ia32}-uncompressed.dmg;
+    rm -rf ${WORKING_DIR}/libdmg-hfsplus ${WORKING_DIR}/gisto-${1}-{x64,ia32}-uncompressed.dmg;
 }
 
+prepare() {
+    cd ${PROJECT_DIR}
+    gulp version_bump --to=${1}
+    gulp dist && gulp dev
+    cd ${BUILD_DIR}/script
+    ./node-webkit-build.sh --build
+    cd ${BUILD_DIR}
+}
+
+if [[ ${1} = "--clean" ]]; then
+    rm -rfv ${WORKING_DIR}
+fi
+
 if [[ ${1} = "--linux" ]]; then
+    prepare ${2};
     mklinux ${2};
 fi
 
 if [[ ${1} = "--osx" ]]; then
+    prepare ${2};
     mkosx ${2};
 fi
 
 if [[ ${1} = "--all" ]]; then
+    prepare ${2};
     mkosx ${2};
     mklinux ${2};
 fi
