e70054aa03125a0abf7c9ccc273ed79ddae8843a
Sasha Khamkov <sanusart@gmail.com>
2014-12-18 21:20:29 +0200

    TOOLS(build/release): add resources and EXE building
    - building windows installers
    - NSIS script
    - cleaned not needed files

--- build/release.sh
+++ build/release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ### USAGE ###
-# ../release.sh --linux 0.2.6b
+# ./release.sh --linux 0.2.6b
 
 # Exit on error
 set -e
@@ -9,6 +9,7 @@ set -e
 BUILD_DIR=`pwd`
 WORKING_DIR="${BUILD_DIR}/TMP"
 PROJECT_DIR=`dirname ${BUILD_DIR}`
+VERSION="${1}"
 
 if [[ ! -f "${WORKING_DIR}" ]]; then
     mkdir -p TMP
@@ -16,14 +17,24 @@ fi
 
 cd TMP
 
-architechture="x64 ia32"
+architechture="ia32 x64"
+
+usage() {
+    printf "\n--- Gisto release script ----------------------------\n\n"
+    printf "\tBuild installers for Windows, Linux and OSX\n"
+    printf "\n--- USAGE -------------------------------------------\n\n"
+    printf "\tBuilding: \n\t$ ./release.sh --all [--linux|--osx|--windows|--all] 0.2.6b\n"
+    printf "\tCleaning: \n\t$ ./release.sh --clean\n"
+    printf "\tHelp: \n\t$ ./release.sh --help"
+    printf "\n-----------------------------------------------------\n"
+}
 
 mklinux () {
     for arch in ${architechture[@]}; do
         cp -r ${BUILD_DIR}/resources/linux/Gisto-X.X.X-Linux ${WORKING_DIR} && mv ${WORKING_DIR}/Gisto-X.X.X-Linux ${WORKING_DIR}/Gisto-${1}-linux_${arch}
-        mv ${BUILD_DIR}/script/TMP/linux-${arch}/latest-git/gisto ${BUILD_DIR}/script/TMP/linux-${arch}/latest-git/gisto-bin
+        mv -T ${BUILD_DIR}/script/TMP/linux-${arch}/latest-git/gisto ${BUILD_DIR}/script/TMP/linux-${arch}/latest-git/gisto-bin
         cp -r ${BUILD_DIR}/script/TMP/linux-${arch}/latest-git/* ${WORKING_DIR}/Gisto-${1}-linux_${arch}/gisto
-        tar -C ${WORKING_DIR} -czf ${WORKING_DIR}/Gisto-${1}-linux_${arch}.tar.gz Gisto-${1}-linux_${arch}
+        tar -C ${WORKING_DIR} -czf ${WORKING_DIR}/Gisto-${1}-Linux-${arch}.tar.gz Gisto-${1}-linux_${arch}
         printf "\nDone Linux ${arch}\n"
     done
     # clean
@@ -40,7 +51,7 @@ mkosx () {
         ln -s /Applications Applications && mv Applications ${BUILD_DIR}/script/TMP/osx-${arch}/latest-git
         cp -r ${BUILD_DIR}/resources/osx/.background ${BUILD_DIR}/resources/osx/.DS_Store ${BUILD_DIR}/script/TMP/osx-${arch}/latest-git
         genisoimage -D -V "Gisto ${1}" -no-pad -r -apple -o ${WORKING_DIR}/gisto-${1}-${arch}-uncompressed.dmg ${BUILD_DIR}/script/TMP/osx-${arch}/latest-git
-        ${WORKING_DIR}/libdmg-hfsplus/dmg/dmg dmg ${WORKING_DIR}/gisto-${1}-${arch}-uncompressed.dmg ${WORKING_DIR}/gisto-${1}-OSX-${arch}.dmg
+        ${WORKING_DIR}/libdmg-hfsplus/dmg/dmg dmg ${WORKING_DIR}/gisto-${1}-${arch}-uncompressed.dmg ${WORKING_DIR}/Gisto-${1}-OSX-${arch}.dmg
         printf "\nDone OSX ${arch}\n"
     done
     rm -rf ${WORKING_DIR}/libdmg-hfsplus ${WORKING_DIR}/gisto-${1}-{x64,ia32}-uncompressed.dmg;
@@ -48,13 +59,29 @@ mkosx () {
 
 mkwindows() {
     for arch in ${architechture[@]}; do
-        if [[ ! -d "${WORKING_DIR}/installjammer" ]]; then
-            git clone https://github.com/damoncourtney/installjammer.git
-            cd installjammer
-            git checkout v1.2.14
-        fi
+        # NOTE need to 
+        # - see how to compile nsis bins here upon release
+        # - remove locals folder
+        # - see how to separate x64 and ia32 in installer
         cd ${WORKING_DIR}
-        ./installjammer/installjammer -DAppName Gisto --quick-build --platform Windows --build win_gisto.mpi
+        cp -r ${BUILD_DIR}/resources/windows/gisto.nsi ${WORKING_DIR}
+        cp -r ${PROJECT_DIR}/app/icon.ico ${BUILD_DIR}/script/TMP/win-${arch}/latest-git/
+        # Replce paths and vars in nsi script
+        replace \
+            GISTO_REPLACE_VERSION ${1} \
+            GISTO_REPLACE_EXE_NAME Gisto-${1}-Windows-${arch}.exe \
+            GISTO_REPLACE_LICENSE ${PROJECT_DIR}/LICENSE \
+            GISTO_REPLACE_INC_FILE_1 ${BUILD_DIR}/script/TMP/win-${arch}/latest-git/gisto.exe \
+            GISTO_REPLACE_INC_FILE_2 ${BUILD_DIR}/script/TMP/win-${arch}/latest-git/icudtl.dat \
+            GISTO_REPLACE_INC_FILE_3 ${BUILD_DIR}/script/TMP/win-${arch}/latest-git/libEGL.dll \
+            GISTO_REPLACE_INC_FILE_4 ${BUILD_DIR}/script/TMP/win-${arch}/latest-git/libGLESv2.dll \
+            GISTO_REPLACE_INC_FILE_5 ${BUILD_DIR}/script/TMP/win-${arch}/latest-git/nw.pak \
+            GISTO_REPLACE_INC_FILE_6 ${BUILD_DIR}/script/TMP/win-${arch}/latest-git/d3dcompiler_46.dll \
+            GISTO_REPLACE_INC_FILE_ICO ${PROJECT_DIR}/app/icon.ico -- gisto.nsi;
+        makensis gisto.nsi
+        # Clean a bit
+        rm -rf ${WORKING_DIR}/gisto.nsi;
+        printf "\nDone Windows ${arch}\n"
     done
 }
 
@@ -67,8 +94,12 @@ prepare() {
     cd ${BUILD_DIR}
 }
 
+if [[ ${1} = "--help" || ${1} = "-h" ]]; then
+    usage;
+fi
+
 if [[ ${1} = "--clean" ]]; then
-    rm -rfv ${WORKING_DIR}
+    rm -rf ${WORKING_DIR}
 fi
 
 if [[ ${1} = "--linux" ]]; then
