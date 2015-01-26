b6eddda556a1d3e3159e0f0a6c7ff5f7fe9c097e
Sasha Khamkov <sanusart@gmail.com>
2015-01-26 22:23:09 +0200

    TOOLS(CI): add drone.io script to the repo
     - add drone.io CI script to the repo, it's better to have it in git and execute it as `./droneIO.sh`

---
+++ droneIO.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Start
+ENV_VAR_GISTO_VERSION="0.2.6b"
+ENV_VAR_NW_VERSION="0.11.6"
+ENV_VAR_GISTO_COMMIT_MESSAGE="$(git log --format='%T%n>>> %B' --since=1am --graph)"
+DATE_NOW=$(date +"%Y%m%d")
+
+git submodule init
+git submodule update
+
+# Dependencies
+sudo apt-get install zip unzip nsis rsync ssh-askpass
+npm install --silent
+
+# Prepare dist
+$DRONE_BUILD_DIR/node_modules/gulp/bin/gulp.js dist
+
+# Write config.json
+echo "{\"client_id\":\"$ENV_VAR_GISTO_CLIENT_ID\",\"client_secret\":\"$ENV_VAR_GISTO_CLIENT_SECRET\",\"server_token\":\"$ENV_VAR_GISTO_SERVER_TOKEN\"}" >> dist/config.json
+
+# Build
+${DRONE_BUILD_DIR}/build/script/nwjs-build.sh --src=${DRONE_BUILD_DIR}/dist --name=gisto --win-icon=${DRONE_BUILD_DIR}/app/icon.ico --osx-icon=${DRONE_BUILD_DIR}/build/resources/osx/gisto.icns --osx-plist=${DRONE_BUILD_DIR}/build/resources/osx/Info.plist --nw=${ENV_VAR_NW_VERSION} --libudev --build
+
+# Prepare
+printf "### git commits for: ${DATE_NOW}\n${ENV_VAR_GISTO_COMMIT_MESSAGE}\n\n[See on GitHub](https://github.com/Gisto/Gisto/commit/${DRONE_COMMIT})" >> ${DRONE_BUILD_DIR}/build/script/TMP/output/_h5ai.header.md
+mv ${DRONE_BUILD_DIR}/build/script/TMP/output ${DRONE_BUILD_DIR}/build/script/TMP/${DATE_NOW}
+cd ${DRONE_BUILD_DIR}/build/script/TMP && ln -s ${DATE_NOW} latest
+
+# Sync to nightly server
+rsync -avc -e ssh ${DRONE_BUILD_DIR}/build/script/TMP/${DATE_NOW} ${DRONE_BUILD_DIR}/build/script/TMP/latest ${ENV_VAR_NIGHTLY_SERVER}:/home/gistoapp/public_html/
+
+# DONE :)
+
