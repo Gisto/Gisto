1aad2402d02b19c0b1f070c5f6fd8a4fcfd8613f
Sasha Khamkov <sanusart@gmail.com>
2015-01-28 19:26:46 +0200

    TOOLS(CI): drone.io add cmd switches for versions
     - for Gisto version
     - for nw.js version

--- droneIO.sh
+++ droneIO.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # Start
-ENV_VAR_GISTO_VERSION="0.2.6b"
-ENV_VAR_NW_VERSION="0.11.6"
+
+# Vars
+ENV_VAR_GISTO_VERSION="0.2.6b" # or set with `droneIO.sh --version=0.2.6b`
+ENV_VAR_NWJS_VERSION="0.11.6" # or set with `droneIO.sh --nwjs=0.11.6`
 ENV_VAR_GISTO_COMMIT_MESSAGE="$(git log --format='#### %B%n[View on GitHub](https://github.com/Gisto/Gisto/commit/%H)%n%n---' --since=1am)"
 DATE_NOW=$(date +"%Y%m%d")
 
+# Pull/update submodules
 git submodule init
 git submodule update
 
@@ -20,7 +23,7 @@ $DRONE_BUILD_DIR/node_modules/gulp/bin/gulp.js dist
 echo "{\"client_id\":\"$ENV_VAR_GISTO_CLIENT_ID\",\"client_secret\":\"$ENV_VAR_GISTO_CLIENT_SECRET\",\"server_token\":\"$ENV_VAR_GISTO_SERVER_TOKEN\"}" >> dist/config.json
 
 # Build
-${DRONE_BUILD_DIR}/build/script/nwjs-build.sh --src=${DRONE_BUILD_DIR}/dist --name=gisto --win-icon=${DRONE_BUILD_DIR}/app/icon.ico --osx-icon=${DRONE_BUILD_DIR}/build/resources/osx/gisto.icns --osx-plist=${DRONE_BUILD_DIR}/build/resources/osx/Info.plist --nw=${ENV_VAR_NW_VERSION} --libudev --build
+${DRONE_BUILD_DIR}/build/script/nwjs-build.sh --src=${DRONE_BUILD_DIR}/dist --name=gisto --win-icon=${DRONE_BUILD_DIR}/app/icon.ico --osx-icon=${DRONE_BUILD_DIR}/build/resources/osx/gisto.icns --osx-plist=${DRONE_BUILD_DIR}/build/resources/osx/Info.plist --nw=${ENV_VAR_NWJS_VERSION} --libudev --build
 
 # Prepare
 printf "### git commit(s) from: ${DATE_NOW}\n\n---\n\n${ENV_VAR_GISTO_COMMIT_MESSAGE}" >> ${DRONE_BUILD_DIR}/build/script/TMP/output/_h5ai.header.md
@@ -29,6 +32,28 @@ cd ${DRONE_BUILD_DIR}/build/script/TMP && ln -s ${DATE_NOW} latest
 
 # Sync to nightly server
 rsync -avc -e ssh ${DRONE_BUILD_DIR}/build/script/TMP/${DATE_NOW} ${DRONE_BUILD_DIR}/build/script/TMP/latest ${ENV_VAR_NIGHTLY_SERVER}:/home/gistoapp/public_html/
+
+### CMD Arguments
+while true; do
+  case $1 in
+    --version )
+        ENV_VAR_GISTO_VERSION="${1#*=}";
+        shift
+        ;;
+    --nwjs=* )
+        ENV_VAR_NWJS_VERSION="${1#*=}";
+        shift
+        ;;
+    -- )
+        shift;
+        break
+        ;;
+    -* )
+        printf 'Hmmm, unknown option: "%s".\n' "${1}";
+        exit 0
+        ;;
+  esac
+done
 
 # DONE :)
 
