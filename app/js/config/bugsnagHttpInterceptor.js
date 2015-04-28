6b4c5da7ef3b5110417901f5d2593aaa57502294
Maayan Glikser <maayan@glikm.com>
2015-04-28 22:27:05 +0300

    IMPROVE (bugsnag): Stop bugsnag to send AJAX errors for user errors

--- app/js/config/bugsnagHttpInterceptor.js
+++ app/js/config/bugsnagHttpInterceptor.js
@@ -15,7 +15,7 @@
         };
 
         function handleError(rejection) {
-            if (!isGitHubStarNotFound(rejection)) {
+            if (!shouldNotSendError(rejection)) {
                 bugsnag.notify("AjaxError", rejection.status + ' on ' + rejection.config.url, {
                     request: {
                         status: rejection.status,
@@ -35,8 +35,27 @@
             return $q.reject(rejection);
         }
 
+        function shouldNotSendError(rejection) {
+            return isGitHubStarNotFound(rejection) &&
+                    isConnectionTimeout(rejection) &&
+                    isAuthorizationError(rejection) &&
+                    isRateLimitReached(rejection);
+        }
+
         function isGitHubStarNotFound(rejection) {
             return rejection.status === 404 && rejection.config.url.indexOf('/star') > -1
+        }
+
+        function isConnectionTimeout(rejection) {
+            return rejection.status === 0;
+        }
+
+        function isAuthorizationError(rejection) {
+            return rejection.status === 401;
+        }
+
+        function isRateLimitReached(rejection) {
+            return rejection.headers()['x-ratelimit-remaining'] === 0;
         }
     }
 })();
