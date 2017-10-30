19b45963d1fb4273f197c2abbd39e3ec7dfb8eb3
Sasha Khamkov <sanusart@gmail.com>
2017-10-30 22:45:30 +0200

    FIX (doc): add usage in CopyToClipBoard directive

--- src/app/directives/copy-to-clip-board.directive.ts
+++ src/app/directives/copy-to-clip-board.directive.ts
@@ -1,5 +1,9 @@
 import { Directive, HostListener, Input } from '@angular/core';
 
+/**
+ * @Usage: <a CopyToClipBoard string="I am to be copied">Copy to Clipboard</a>
+ */
+
 @Directive({
   selector: '[CopyToClipBoard]'
 })
@@ -8,11 +12,12 @@ export class CopyToClipBoardDirective {
   @Input() string: string;
 
   @HostListener('click') onClick() {
-    function handleCopy (event) {
+    function handleCopy(event) {
       event.clipboardData.setData('text/plain', this.string);
       event.preventDefault();
       document.removeEventListener('copy', handleCopy, true);
     }
+
     document.addEventListener('copy', handleCopy.bind(this), true);
     document.execCommand('copy');
   }
