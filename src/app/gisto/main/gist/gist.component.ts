90cc720ee560ee8a8a93d759d386548a6e813406
Sasha Khamkov <sanusart@gmail.com>
2017-11-03 00:40:08 +0200

    FIX (base href): fix base href

--- src/app/gisto/main/gist/gist.component.ts
+++ src/app/gisto/main/gist/gist.component.ts
@@ -44,14 +44,4 @@ export class GistComponent {
     this.gistStore.changeLocalDataContent(filename, value);
   }
 
-  copyToClipboard(text) {
-    function handler (event){
-      event.clipboardData.setData('text/plain', text);
-      event.preventDefault();
-      document.removeEventListener('copy', handler, true);
-    }
-    document.addEventListener('copy', handler, true);
-    document.execCommand('copy');
-  }
-
 }
