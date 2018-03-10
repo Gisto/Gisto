d095df17bb4548beb566ecb48925c3284e199f50
Sasha Khamkov <sanusart@gmail.com>
2018-03-10 09:13:03 +0200

    FIX (updater): events

--- src/electron/electron.dev.js
+++ src/electron/electron.dev.js
@@ -102,22 +102,27 @@ app.on('ready', createWindow);
 
 autoUpdater.on('checking-for-update', () => {
   sendStatusToWindow('Checking for update...');
-})
+});
+
 autoUpdater.on('update-available', (info) => {
   sendStatusToWindow('Update available.');
-})
+});
+
 autoUpdater.on('update-not-available', (info) => {
   sendStatusToWindow('Update not available.');
-})
+});
+
 autoUpdater.on('error', (err) => {
   sendStatusToWindow('Error in auto-updater. ' + err);
-})
+});
+
 autoUpdater.on('download-progress', (progressObj) => {
   let log_message = "Download speed: " + progressObj.bytesPerSecond;
   log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
   log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
   sendStatusToWindow(log_message);
-})
+});
+
 autoUpdater.on('update-downloaded', (info) => {
   sendStatusToWindow('Update downloaded');
 });
--- src/electron/electron.prod.js
+++ src/electron/electron.prod.js
@@ -3,8 +3,49 @@ const path = require('path');
 const url = require('url');
 require('./oauth2');
 
+const log = require('electron-log');
+const { autoUpdater } = require("electron-updater");
+
+const { checkForUpdates } = require('./updater');
+
 let win;
 let splash;
+
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+log.info('App starting...');
+
+let template = [];
+if (process.platform === 'darwin') {
+  // OS X
+  const name = app.getName();
+  template.unshift({
+    label: name,
+    submenu: [
+      {
+        label: 'About ' + name,
+        role: 'about'
+      },
+      {
+        label: 'Console',
+        click() {
+          win.webContents.openDevTools();
+        }
+      },
+      {
+        label: 'Check for updates',
+        click() {
+          checkForUpdates();
+        }
+      },
+      {
+        label: 'Quit',
+        accelerator: 'Command+Q',
+        click() { app.quit(); }
+      },
+    ]
+  })
+}
 
 const createWindow = () => {
   protocol.interceptFileProtocol('file', function(req, callback) {
@@ -38,14 +79,46 @@ const createWindow = () => {
   win.once('ready-to-show', () => {
     splash.destroy();
     win.show();
+
+    const menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
   });
 
   win.on('closed', () => {
     win = null;
   });
+
+  autoUpdater.checkForUpdatesAndNotify();
 };
 
 app.on('ready', createWindow);
+
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
+});
+
+autoUpdater.on('update-available', (info) => {
+  sendStatusToWindow('Update available.');
+});
+
+autoUpdater.on('update-not-available', (info) => {
+  sendStatusToWindow('Update not available.');
+});
+
+autoUpdater.on('error', (err) => {
+  sendStatusToWindow('Error in auto-updater. ' + err);
+});
+
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  sendStatusToWindow(log_message);
+});
+
+autoUpdater.on('update-downloaded', (info) => {
+  sendStatusToWindow('Update downloaded');
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
