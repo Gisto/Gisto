7717ae2695fc22faae4073ab5c09964e91afd620
Sasha Khamkov <sanusart@gmail.com>
2018-06-09 21:42:42 +0300

    FEATURE (star): allow star/unstar from sidebar

--- src/components/layout/sidebar/Snippet.js
+++ src/components/layout/sidebar/Snippet.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import styled from 'styled-components';
+import { get } from 'lodash/fp';
 import { HashRouter as Router, NavLink } from 'react-router-dom';
+
 import { removeTags } from 'utils/tags';
+import * as snippetActions from 'actions/snippets';
 import { baseAppColor, bg, lightText } from 'constants/colors';
 import { SIDEBAR_WIDTH } from 'constants/config';
+
 import Icon from 'components/common/Icon';
 
 const StyledNavLink = styled(NavLink)`
@@ -33,31 +38,55 @@ const Title = styled.span`
   font-size: 16px;
 `;
 
-const Snippet = ({ snippet }) => (
-  <Router>
-    <StyledNavLink exact
-                   className="link"
-                   activeClassName="selected"
-                   to={ `/snippet/${snippet.id}` }>
-      <span>
-        <Icon size={ 32 }
+const StyledIcon = styled(Icon)`
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    transform: scale(1.5);
+  }
+`;
+
+const Snippet = ({ snippet, setStar, unsetStar }) => {
+  const starred = get('star', snippet);
+  const toggleStar = (event, id, isStarred) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    return isStarred ? unsetStar(id) : setStar(id);
+  };
+
+  return (
+    <Router>
+      <StyledNavLink exact
+                     className="link"
+                     activeClassName="selected"
+                     to={ `/snippet/${snippet.id}` }>
+        <span>
+          <Icon size={ 32 }
               type={ snippet.public ? 'unlock' : 'lock' }
               color={ lightText }/>
-      </span>
-      <span>
-        <Icon size={ 16 }
-              type={ snippet.star ? 'star-full' : 'star-empty' }
-              color={ lightText }/>
-      </span>
-      <Title>
-        { removeTags(snippet.description) || 'unnamed' }
-      </Title>
-    </StyledNavLink>
-  </Router>
-);
-
-Snippet.propTypes = {
-  snippet: PropTypes.object
+        </span>
+        <span>
+          <StyledIcon size={ 16 }
+                      onClick={ (event) => toggleStar(event, snippet.id, starred) }
+                      type={ snippet.star ? 'star-full' : 'star-empty' }
+                      color={ lightText }/>
+        </span>
+        <Title>
+          {removeTags(snippet.description) || 'unnamed'}
+        </Title>
+      </StyledNavLink>
+    </Router>
+  );
 };
 
-export default Snippet;
+Snippet.propTypes = {
+  snippet: PropTypes.object,
+  setStar: PropTypes.func,
+  unsetStar: PropTypes.func
+};
+
+export default connect(null, {
+  setStar: snippetActions.starSnippet,
+  unsetStar: snippetActions.unStarSnippet
+})(Snippet);
