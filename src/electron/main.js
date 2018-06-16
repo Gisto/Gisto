c567d2d8f11f89a2a1eb87c3ff5c0a9646f1f948
Sasha Khamkov <sanusart@gmail.com>
2018-06-16 15:19:56 +0300

    FIX (npm run): electron wait for webpack to build

--- src/electron/main.js
+++ src/electron/main.js
@@ -4,11 +4,7 @@ import installExtension, { REACT_DEVELOPER_TOOLS, REACT_PERF, REDUX_DEVTOOLS } f
 
 let mainWindow;
 
-console.log('\x1b[37m\x1b[41m', 'LOG ', '\x1b[0m', process.env.NODE_ENV);
-
-// if (process.env.NODE_ENV === 'development') {
-//   require('electron-reload')(__dirname);
-// }
+console.log('\x1b[37m\x1b[41m', ' ENV ', '\x1b[0m', process.env.NODE_ENV);
 
 const installExtensions = async () => installExtension([
   REACT_DEVELOPER_TOOLS.id,
