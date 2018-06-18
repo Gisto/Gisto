1acd1fda6920cfc55b1679a0dfaa565d56f3da65
Sasha Khamkov <sanusart@gmail.com>
2018-06-18 18:35:31 +0300

    TEST (common): add more tests for common

--- jest-setup.js
+++ jest-setup.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Enzyme, { shallow, render, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+
 // React 16 Enzyme adapter
 Enzyme.configure({ adapter: new Adapter() });
 // Make Enzyme functions available in all test files without importing
