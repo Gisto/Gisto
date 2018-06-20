7a4fb5987ac5b94bd207a364f814dab5083be5f7
Sasha Khamkov <sanusart@gmail.com>
2018-06-20 23:06:21 +0300

    FEATURE (search): add supersearch

--- src/components/layout/App.js
+++ src/components/layout/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { baseAppColor } from 'constants/colors';
+import * as Mousetrap from 'mousetrap';
+import SuperSearch from 'components/layout/SuperSearch';
 
 const AppWrapper = styled.div`
   display: flex;
@@ -11,9 +13,35 @@ const AppWrapper = styled.div`
   color: ${baseAppColor};
 `;
 
-const App = (props) => (
-  <AppWrapper>{ props.children }</AppWrapper>
-);
+export class App  extends React.Component {
+  state = {
+    showSuperSearch: false
+  };
+
+  componentDidMount() {
+    Mousetrap.bind(['shift shift', 'ctrl+f'], this.toggleSuperSesrch);
+  }
+  componentWillUnmount() {
+    Mousetrap.unbind(['shift shift', 'ctrl+f'], this.toggleSuperSesrch);
+  }
+
+  toggleSuperSesrch = () => {
+    this.setState({
+      showSuperSearch: this.state.showSuperSearch = !this.state.showSuperSearch
+    });
+  };
+
+  render() {
+    return (
+      <AppWrapper>
+        { this.props.children }
+        { this.state.showSuperSearch && (
+          <SuperSearch toggleSuperSesrch={ () => this.toggleSuperSesrch() }/>
+        ) }
+      </AppWrapper>
+    );
+  }
+}
 
 App.propTypes = {
   children: PropTypes.node
