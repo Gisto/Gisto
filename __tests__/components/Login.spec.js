bc1679c0b8dbf89fed34e8032e9abcf673aa5307
Sasha Khamkov <sanusart@gmail.com>
2018-07-03 02:42:35 +0300

    TEST (login): add more test for login

--- __tests__/components/Login.spec.js
+++ __tests__/components/Login.spec.js
@@ -1,20 +1,52 @@
 import { LogIn } from 'components/LogIn';
 
 const propSetup = (props) => ({
+  loginBasic: jest.fn(),
+  loginWithToken: jest.fn(),
+  twoFactorAuth: '',
+  loading: false,
   ...props
 });
 
 const setup = (props) => shallow(<LogIn { ...propSetup(props) }/>);
 
 describe('COMPONENTS - <LogIn>', () => {
-  test('render LogIn', () => {
-    const component = setup({
-      loginBasic: jest.fn(),
-      loginWithToken: jest.fn(),
-      twoFactorAuth: '',
-      loading: false
-    });
+  test('render LogIn basic', () => {
+    const component = setup();
 
+    expect(component).toMatchSnapshot();
+  });
+
+  test('render LogIn token', () => {
+    const component = setup();
+
+    component.setState({
+      loginType: {
+        token: true
+      } 
+    });
+    expect(component).toMatchSnapshot();
+  });
+
+  test('render LogIn enterprise', () => {
+    const component = setup();
+
+    component.setState({
+      loginType: {
+        enterprise: true
+      }
+    });
+    expect(component).toMatchSnapshot();
+  });
+
+  test('render LogIn github oauth2', () => {
+    const component = setup();
+
+    component.setState({
+      loginType: {
+        github: true
+      }
+    });
     expect(component).toMatchSnapshot();
   });
 });
