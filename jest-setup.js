eac2cb87f19665f3528d0814ae9837a59e97bd60
Sasha Khamkov <sanusart@gmail.com>
2018-07-03 03:22:02 +0300

    TEST (login): add click tests for login

--- jest-setup.js
+++ jest-setup.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Enzyme, { shallow, render, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import sinon from 'sinon';
 
 // React 16 Enzyme adapter
 Enzyme.configure({ adapter: new Adapter() });
@@ -9,3 +10,4 @@ global.shallow = shallow;
 global.render = render;
 global.mount = mount;
 global.React = React;
+global.sinon = sinon;
