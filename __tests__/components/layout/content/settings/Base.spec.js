362406b76411fba9a436ee63e95e65885eb4b4a3
Sasha Khamkov <sanusart@gmail.com>
2018-08-05 20:47:54 +0300

    TEST (settings): add more tests

--- __tests__/components/layout/content/settings/Base.spec.js
+++ __tests__/components/layout/content/settings/Base.spec.js
@@ -4,12 +4,19 @@ const propSetup = (props) => ({
   ...props
 });
 
-const setup = (props) => mount(<BaseSettings { ...propSetup(props) }/>);
+const setup = (props) => shallow(<BaseSettings { ...propSetup(props) }/>);
 
 describe('COMPONENTS - <BaseSettings>', () => {
   test('render BaseSettings', () => {
     const component = setup();
 
     expect(component).toMatchSnapshot();
+  });
+
+  test('change color', () => {
+    const component = setup();
+
+    expect(component.find('input')).toHaveLength(1);
+    component.find('input').simulate('change', { target: { value: '#ff0000' } });
   });
 });
