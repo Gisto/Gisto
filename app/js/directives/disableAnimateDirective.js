b552887ed5608da95ddc8f73fc88df8f686ecba5
Sasha Khamkov <sanusart@gmail.com>
2018-08-13 08:47:31 +0300

    merge next to master (unrelated-histories)

--- app/js/directives/disableAnimateDirective.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('gisto.directive.disableAnimate', ['ngAnimate']).directive('disableAnimate', function ($animate) {
-    return function($scope, $element) {
-        console.log('disable animate', $scope, $element, $animate);
-        $animate.enabled(false, $element);
-    }
-});
--- app/js/filters/publicOrPrivateFilter.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('gisto.filter.publicOrPrivet', []).filter('publicOrPrivet', function () {
-        return function (input) {
-            return input ? 'unlock' : 'lock';
-        };
-    });
