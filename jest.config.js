ac8e154964c8bd26cddb00e37e05b3d66d16151c
Sasha Khamkov <sanusart@gmail.com>
2018-08-16 22:43:28 +0300

    TOOLS (jest): separate config from package.json

---
+++ jest.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  testURL: 'http://localhost/',
+  modulePaths: [
+    '/src',
+    '<rootDir>'
+  ],
+  moduleFileExtensions: [
+    'js'
+  ],
+  moduleDirectories: [
+    'node_modules',
+    'src'
+  ],
+  setupFiles: [
+    '<rootDir>/test/jest-setup.js',
+    '<rootDir>/__mocks__/electron.js',
+    '<rootDir>/__mocks__/localStorage.js'
+  ],
+  snapshotSerializers: [
+    'enzyme-to-json/serializer'
+  ],
+  moduleNameMapper: {
+    '^.+\\.(css|scss)$': 'identity-obj-proxy',
+    'monaco-editor': '<rootDir>/node_modules/react-monaco-editor',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/test/assetsTransformer.js'
+  },
+  coverageDirectory: './coverage/',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js}',
+    '!src/electron/**',
+    '!src/store/**',
+    '!src/selectors/**',
+    '!src/reducers/root.js',
+    '!src/index.js',
+    '!**/node_modules/**',
+    '!**/build/**',
+    '!**/dist/**'
+  ],
+  coverageReporters: [
+    'lcov'
+  ]
+};
