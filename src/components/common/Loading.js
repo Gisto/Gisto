532cb9b296b93c8c1fd8ad5f6a22b61a9dec86d6
Sasha Khamkov <sanusart@gmail.com>
2018-08-20 08:08:36 +0300

    FIX (loading): fix loading indicator and add it to file loading

--- src/components/common/Loading.js
+++ src/components/common/Loading.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 import { colorDanger } from 'constants/colors';
 
 import Icon from 'components/common/Icon';
 
+const Spinner = styled.div`
+  border: 1px solid ${(props) => props.color ? props.color : '#fff'};
+  border-top: 1px solid rgba(255,255,255,.3);
+  border-radius: 50%;
+  width: 20px;
+  height: 20px;
+  animation: spin 2s linear infinite;
+  align-self: center;
+  vertical-align: middle;
+  display: inline-block;
+  margin: 0 5px 0 0;
 
-const loader = (color) => {
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+`;
+
+const loader = (color, text) => {
   if (window.navigator.onLine) {
     return (
       <React.Fragment>
-        <Icon type="loading" color={ color }/> { 'loading...' }
+        <Spinner color={ color }/> { text }
       </React.Fragment>
     );
   }
@@ -22,9 +40,10 @@ const loader = (color) => {
   );
 };
 
-const Loading = ({ color }) => loader(color);
+const Loading = ({ color, text }) => loader(color, text);
 
 Loading.propTypes = {
+  color: PropTypes.string,
   text: PropTypes.string,
   children: PropTypes.any
 };
