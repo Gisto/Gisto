1de0303e331cc39ca081e1a5f829a621364a44ef
Sasha Khamkov <sanusart@gmail.com>
2018-08-22 10:23:47 +0300

    FIX (tools): changelog

--- change-log-builder.sh
+++ change-log-builder.sh
@@ -2,5 +2,5 @@
 
 COMMITS=$(git log `git describe --tags --abbrev=0`..HEAD --oneline --pretty=format:'*  %s [View](https://github.com/Gisto/Gisto/commit/%H)' --reverse | grep -v Merge)
 
-printf "###vPLACEHOLDER\n${COMMITS}" | cat - CHANGELOG.md > /tmp/temp-gisto-msg && mv /tmp/temp-gisto-msg CHANGELOG.md
+printf "###vPLACEHOLDER\n${COMMITS}\n\n" | cat - CHANGELOG.md > /tmp/temp-gisto-msg && mv /tmp/temp-gisto-msg CHANGELOG.md
 
