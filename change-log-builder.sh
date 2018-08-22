fc9c65152471dcb74eb8acd2bfc2b76e2c0378ea
Sasha Khamkov <sanusart@gmail.com>
2018-08-22 08:07:38 +0300

    test

---
+++ change-log-builder.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+COMMITS=$(git log `git describe --tags --abbrev=0`..HEAD --oneline --pretty=format:'*  %s [View](https://github.com/Gisto/Gisto/commit/%H)' --reverse | grep -v Merge)
+
+echo "###v[PLACEHOLDER]\n${COMMITS}" | cat - CHANGELOG.md > /tmp/temp-gisto-msg && mv /tmp/temp-gisto-msg CHANGELOG.md
+
