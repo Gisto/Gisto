488c6873d052179ac72e03ba7d968fabd6df316a
Sasha Khamkov <sanusart@gmail.com>
2018-09-04 22:50:41 +0300

    FIX (API): header null checking

--- src/middlewares/gitHubAPI.js
+++ src/middlewares/gitHubAPI.js
@@ -28,7 +28,7 @@ const gitHubAPIMiddleware = ({ dispatch }) => {
   return (next) => (action) => {
     const errorHandler = (error, result) => {
       if (error) {
-        if (error.response.headers['x-github-otp']) {
+        if (error.response && error.response.headers['x-github-otp']) {
           setNotification({
             title: 'Two factor authentication',
             body: `${error.response.body.message} (${error.status})`,
