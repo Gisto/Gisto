37ca645e8283c80b4716db598f521c124dd81f35
Sasha Khamkov <sanusart@gmail.com>
2018-09-15 21:11:53 +0300

    Revert "CHORE (update packages): update packages"
    This reverts commit 98f20ffb3de3729790461ad825efcb6e467aa88b.

--- __tests__/reduceres/snippets.spec.js
+++ __tests__/reduceres/snippets.spec.js
@@ -314,7 +314,7 @@ describe('reducer - snippets', () => {
       .toEqual(expected);
   });
 
-  test.skip('should create snippet', () => {
+  test('should create snippet', () => {
     const newSnippet = snippet({ id: 'ddd123', description: 'ddd123 #ddd' });
     const action = {
       type: AT.CREATE_SNIPPET.SUCCESS,
@@ -323,14 +323,12 @@ describe('reducer - snippets', () => {
 
     const initial = initialState({
       snippets: snippets(),
-      starred: ['bbb123'],
-      lastOpenedId: ''
+      starred: ['bbb123']
     });
 
     const expected = initialState({
       snippets: [ ...snippets(), ...snippetStructure(action.payload, initial.starred) ],
-      starred: ['bbb123'],
-      lastOpenedId: ''
+      starred: ['bbb123']
     });
 
     expect(reducer(JSON.stringify(initial), action))
