71229005efb4a53358bc1e58df9ec98d9d2c638f
Sasha Khamkov <sanusart@gmail.com>
2018-09-16 09:34:28 +0300

    TEST (common): add more tests to common components

---
+++ __tests__/components/common/AsciiDoc.spec.js
@@ -0,0 +1,16 @@
+import Asciidoc from 'components/common/AsciiDoc';
+
+const setup = (props) => shallow(<Asciidoc { ...props }/>);
+const text = `bold *constrained* & **un**constrained
+
+italic _constrained_ & __un__constrained
+
+bold italic *_constrained_* & **__un__**constrained`;
+
+describe('COMPONENTS - <Asciidoc>', () => {
+  test('render Asciidoc', () => {
+    const component = setup({ text });
+
+    expect(component).toMatchSnapshot();
+  });
+});
---
+++ __tests__/components/common/Markdown.spec.js
@@ -0,0 +1,37 @@
+import { Markdown } from 'components/common/Markdown';
+
+const setup = (props) => shallow(<Markdown { ...props }/>);
+const text = '#hello' +
+  '\n' +
+  'Paragraph **with** _stuff_' +
+  '\n' +
+  '```' + '\n' +
+  'code' + '\n' +
+  '```' + '\n' +
+  '\n' +
+  '> quote' +
+  '\n' +
+  '[link](https://wwww.gistoapp.com)';
+
+const textWithEmoji = `${text} :100:`;
+
+describe('COMPONENTS - <Markdown>', () => {
+  test('render Markdown', () => {
+    const component = setup({
+      text
+    });
+
+    expect(component).toMatchSnapshot();
+  });
+
+  test('render Markdown with emoji', () => {
+    const component = setup({
+      text: textWithEmoji,
+      emoji: ({
+        100: 'https://dummy-emoji.url'
+      })
+    });
+
+    expect(component).toMatchSnapshot();
+  });
+});
