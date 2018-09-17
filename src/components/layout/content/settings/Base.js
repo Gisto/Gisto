c2a92b7b67c76a3842f8ef30f204326af2990e15
Sasha Khamkov <sanusart@gmail.com>
2018-09-17 22:15:28 +0300

    FIX (settings): add configuration for snippet cache before next fetch

--- src/components/layout/content/settings/Base.js
+++ src/components/layout/content/settings/Base.js
@@ -4,18 +4,35 @@ import styled from 'styled-components';
 import { getSetting, setSetting } from 'utils/settings';
 
 import InputColor from 'components/common/controls/InputColor';
+import Input from 'components/common/controls/Input';
 
 const Label = styled.div``;
 
+const StyledInput = styled(Input)`
+  padding: 0 5px;
+  width: 148px;
+`;
+
 const BaseSettings = () => (
   <div>
-    <h4>color settings:</h4>
+    <h4>Color settings:</h4>
 
     <div>
       <Label>
         Base color:
         <InputColor color={ getSetting('color') }
                     onChange={ (event) => setSetting('color', event.target.value) }/>
+      </Label>
+    </div>
+
+    <h4>General settings:</h4>
+
+    <div>
+      <Label>
+        <span>Snippet cache (seconds):</span>
+        <StyledInput type="number"
+                     value={ getSetting('snippet-fetch-cache-in-seconds', 100) }
+                     onChange={ (event) => setSetting('snippet-fetch-cache-in-seconds', event.target.value) }/>
       </Label>
     </div>
   </div>
