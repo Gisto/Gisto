59a3b83119c88eff7a3bd162919dec0733413fd8
Sasha Khamkov <sanusart@gmail.com>
2018-09-22 12:26:49 +0300

    FIX (ga): events

--- src/utils/ga.js
+++ src/utils/ga.js
@@ -3,10 +3,13 @@ import { isElectron } from 'utils/electron';
 
 export const gaPage = (page) => {
   if (isElectron) {
+    const cid = localStorage.getItem('unique-app-id');
+
     const Analytics  = require('electron-google-analytics').default;
     const analytics = new Analytics(GAUA);
+    const cookieDomain = 'https://web.gistoapp.com';
 
-    return analytics.pageview('https://web.gistoapp.com', page, 'Gisto');
+    return analytics.pageview(cookieDomain, page, 'Gisto', cid);
   }
 
   gtag('config', GAUA, {
@@ -18,14 +21,16 @@ export const gaPage = (page) => {
 };
 
 export const gaEvent = ({
-  category, action, label, value, cid
+  category, action, label, value
 }) => {
   if (isElectron) {
+    const cid = localStorage.getItem('unique-app-id');
     const Analytics  = require('electron-google-analytics').default;
     const analytics = new Analytics(GAUA);
 
     return analytics.event(category, action, { evLabel: label, evValue: value, clientID: cid });
   }
+
   gtag('event', action, {
     event_category: category,
     event_label: label,
