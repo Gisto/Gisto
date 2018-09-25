df81269813088d88ff8f243a2a1cb2f70a83e841
Sasha Khamkov <sanusart@gmail.com>
2018-09-25 07:14:23 +0300

    FIX (tags): wrap long tags

--- src/components/common/Taglist.js
+++ src/components/common/Taglist.js
@@ -15,8 +15,22 @@ const Pill = styled.span`
   color: ${baseAppColor};
   padding: 5px;
   border-radius: 3px;
+  position: relative;
+  text-overflow: clip;
+  overflow: hidden;
   &:hover {
     border: 1px solid ${baseAppColor};
+  }
+  
+  &:after {
+    content: "";
+    width: 30px;
+    height: 20px;
+    position: absolute;
+    top: 3px;
+    right: 0;
+    background: -webkit-gradient(linear,left top,right top,color-stop(0%,rgba(255,255,255,0)),color-stop(56%,rgba(255,255,255,1)),color-stop(100%,rgba(255,255,255,1)));
+    background: -webkit-linear-gradient(left,rgba(255,255,255,0) 0%,rgba(255,255,255,1) 56%,rgba(255,255,255,1) 100%);
   }
 `;
 
@@ -46,7 +60,7 @@ export const Taglist = ({
   return (
     <React.Fragment>
       { map((tag) => (
-        <Pill key={ tag } onClick={ () => searchByTags(tag) }>
+        <Pill key={ tag } onClick={ () => searchByTags(tag) } title={ tag }>
           { tag }
         </Pill>
       ), getTags()) }
