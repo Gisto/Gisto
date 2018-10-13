056df3bd97e861efaf1e15747bf169fa7d73e479
Sasha Khamkov <sanusart@gmail.com>
2018-10-13 18:36:08 +0300

    TESTS (actions+layput): add more tests

---
+++ __tests__/components/layout/Updater.js
@@ -0,0 +1,15 @@
+import Updater from 'components/layout/Updater';
+
+const propSetup = (props) => ({
+  ...props
+});
+
+const setup = (props) => shallow(<Updater { ...propSetup(props) }/>);
+
+describe('COMPONENTS - <Updater>', () => {
+  test('render Updater', () => {
+    const component = setup();
+
+    expect(component).toMatchSnapshot();
+  });
+});
