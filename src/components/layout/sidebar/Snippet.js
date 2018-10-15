439ed1efd9d58d24b079f4594e4a0d0a8e6f402b
Sasha Khamkov <sanusart@gmail.com>
2018-10-15 23:16:12 +0300

    FEATURE (contextmenu): add context menu (right click) to snippets list

--- src/components/layout/sidebar/Snippet.js
+++ src/components/layout/sidebar/Snippet.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
@@ -11,6 +11,7 @@ import { baseAppColor, bg, lightText } from 'constants/colors';
 import { SIDEBAR_WIDTH } from 'constants/config';
 
 import Icon from 'components/common/Icon';
+import { isElectron } from 'utils/electron';
 
 const StyledNavLink = styled(NavLink)`
   background: ${baseAppColor};
@@ -52,47 +53,101 @@ const StyledIcon = styled(Icon)`
   }
 `;
 
-export const Snippet = ({ snippet, setStar, unsetStar }) => {
-  const starred = get('star', snippet);
-  const toggleStar = (event, id, isStarred) => {
+export class Snippet extends Component {
+  toggleStar = (event) => {
     event.preventDefault();
     event.stopPropagation();
 
-    return isStarred ? unsetStar(id) : setStar(id);
+    const { setStar, unsetStar, snippet } = this.props;
+    const isStarred = get('star', snippet);
+
+    return isStarred ? unsetStar(snippet.id) : setStar(snippet.id);
   };
 
-  return (
-    <Router>
-      <StyledNavLink exact
-                     className="link"
-                     activeClassName="selected"
-                     to={ `/snippet/${snippet.id}` }>
-        <span>
-          <StyledIcon size={ 24 }
+  deleteSnippet = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    const { snippet } = this.props;
+    // eslint-disable-next-line no-restricted-globals, no-alert
+    const sure = confirm(`Are you sure you want to delete "${snippet.description}"?`);
+
+    if (sure === true) {
+      this.props.deleteSnippet(snippet.id);
+
+      return true;
+    }
+
+    return false;
+  };
+
+  onContextMenu = (event, snippet) => {
+    event.preventDefault();
+    event.stopPropagation();
+    event.persist();
+
+    const { remote } = require('electron');
+    const { Menu, MenuItem } = remote;
+    const menu = new Menu();
+
+    menu.append(new MenuItem({
+      label: snippet.star ? 'Un-star' : 'Star',
+      click: () => this.toggleStar(event)
+    }));
+
+    menu.append(new MenuItem({
+      type: 'separator'
+    }));
+
+    menu.append(new MenuItem({
+      label: 'Delete',
+      click: () => this.deleteSnippet(event)
+    }));
+
+    menu.popup({ window: remote.getCurrentWindow() });
+  };
+
+  render() {
+    const { snippet } = this.props;
+
+    return (
+      <Router>
+        <StyledNavLink exact
+                       className="link"
+                       activeClassName="selected"
+                       to={ `/snippet/${snippet.id}` }
+                       onContextMenu={ isElectron
+                         ? (event) => this.onContextMenu(event, snippet)
+                         : null }>
+          <span>
+            <StyledIcon size={ 24 }
                       type={ snippet.public ? 'unlock' : 'lock' }
                       color={ lightText }/>
-        </span>
-        <span>
-          <StyledIcon size={ 16 }
-                      onClick={ (event) => toggleStar(event, snippet.id, starred) }
+          </span>
+          <span>
+            <StyledIcon size={ 16 }
+                      onClick={ this.toggleStar }
                       type={ snippet.star ? 'star-full' : 'star-empty' }
                       color={ lightText }/>
-        </span>
-        <Title>
-          {removeTags(snippet.description) || 'unnamed'}
-        </Title>
-      </StyledNavLink>
-    </Router>
-  );
-};
+          </span>
+          <Title>
+            { removeTags(snippet.description) || 'unnamed' }
+          </Title>
+        </StyledNavLink>
+      </Router>
+    );
+  }
+}
 
 Snippet.propTypes = {
   snippet: PropTypes.object,
   setStar: PropTypes.func,
-  unsetStar: PropTypes.func
+  unsetStar: PropTypes.func,
+  deleteSnippet: PropTypes.func
 };
 
 export default connect(null, {
   setStar: snippetActions.starSnippet,
-  unsetStar: snippetActions.unStarSnippet
+  unsetStar: snippetActions.unStarSnippet,
+  deleteSnippet: snippetActions.deleteSnippet
 })(Snippet);
