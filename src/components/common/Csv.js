1664f0d10ae6e727e4628416707c1c1830af294e
Sasha Khamkov <sanusart@gmail.com>
2018-10-25 00:57:07 +0300

    FEATURE (editor): preview for csv files

---
+++ src/components/common/Csv.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { map } from 'lodash/fp';
+import styled, { css } from 'styled-components';
+import Papa from 'papaparse';
+
+const TableWrapper = styled.div`
+  overflow: auto;
+  ${(props) => props.height ? `height: ${props.height}px` : 'auto'};
+`;
+
+const Table = styled.table`
+  margin: 0; 
+  padding: 0;
+  border-collapse: collapse;
+  table-layout: fixed;
+  min-width: 100%;
+  tr:nth-child(even) {
+    background-color: ${(props) => props.theme.bg};
+  }
+  
+  tr:hover {
+    background-color: ${(props) => props.theme.headerBgLightest};
+  }
+`;
+const Row = styled.tr`
+  :first-of-type {
+    font-weight: bold;
+  }
+`;
+
+const IndexCell = css`
+  margin: 0;
+  padding: 10px;
+  background: #fff;
+  text-align: center;
+  box-shadow: 0 7px 5px #ccc;
+  width: 32px;
+  display: block;
+  position: relative;
+  
+  tr:hover & {
+    font-weight: bold;
+  }
+`;
+
+const Cell = styled.td`
+  margin: 0; 
+  padding: 10px;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+  ${(props) => props.isIndex ? `${IndexCell}` : ''};
+  max-width: 300px;
+`;
+
+const Csv = ({ text, className, height }) => {
+  const csvData = Papa.parse(text);
+
+  return (
+    <TableWrapper className={ `csv-body ${className}` } height={ height }>
+      <Table>
+        <tbody>
+          { csvData.data && csvData.data.map((row, index) => (
+            <Row>
+              <Cell isIndex 
+                  key={ `row-${index + 1}` }>
+                { index + 1 }
+              </Cell>
+              { map((cell) => (
+                <Cell title={ cell }>{ cell }</Cell>
+              ), row) }
+            </Row>
+          )) }
+        </tbody>
+      </Table>
+    </TableWrapper>
+  );
+};
+
+Csv.propTypes = {
+  text: PropTypes.string,
+  className: PropTypes.string,
+  height: PropTypes.number
+};
+
+export default Csv;
