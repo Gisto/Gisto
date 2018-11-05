7da85310bdb8993111de2a86d18cf0feb2004562
Sasha Khamkov <sanusart@gmail.com>
2018-11-05 20:02:25 +0200

    CHORE (packages): upgrade npm packages
    - upgrade npm packages
    - fix braking change in tint of polished

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,19 +1,8 @@
 import { getSetting } from 'utils/settings';
 import { tint } from 'polished';
 
-export const baseAppColor = getSetting('color', '#3F84A8');
-export const borderColor = '#ccc';
-export const disabledColor = borderColor;
-export const lightBorderColor = '#f2f2f2';
-export const lightText = '#fff';
-export const textColor = '#555';
-export const boxShadow = textColor;
-export const colorDanger = 'tomato';
-export const colorSuccess = 'cadetblue';
-export const colorWarning = '#e0b01e';
-export const headerColor = lightText;
-export const headerBgLightest = tint(0.25, baseAppColor);
-export const bg = tint(0.10, baseAppColor);
+export const tintBackground = (color) => tint(0.90, color);
+export const tintHeaderBgLightest = (color) => tint(0.75, color);
 
 export const theme = {
   baseAppColor: getSetting('color', '#3F84A8'),
@@ -27,6 +16,6 @@ export const theme = {
   colorSuccess: 'cadetblue',
   colorWarning: '#e0b01e',
   headerColor: '#fff',
-  headerBgLightest: tint(0.25, getSetting('color', '#3F84A8')),
-  bg: tint(0.10, getSetting('color', '#3F84A8'))
+  headerBgLightest: tintHeaderBgLightest(getSetting('color', '#3F84A8')),
+  bg: tintBackground(getSetting('color', '#3F84A8'))
 };
