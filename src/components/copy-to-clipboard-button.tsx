74e5b9599c469ace3a0db5c5290198903a5b82fa
Sasha Khamkov <sanusart@gmail.com>
2025-01-25 20:57:17 +0200

    fix preview json

---
+++ src/components/copy-to-clipboard-button.tsx
@@ -0,0 +1,22 @@
+import { Clipboard, ClipboardCheck } from 'lucide-react';
+import { useState } from 'react';
+
+import { Button } from '@/components/ui/button.tsx';
+import { copyToClipboard } from '@/lib/utils.ts';
+
+export const CopyToClipboardButton = ({ text }: { text: string }) => {
+  const [copyActive, setCopyActive] = useState<boolean>(false);
+  return (
+    <Button
+      variant="link"
+      size="sm"
+      onClick={() => {
+        copyToClipboard(text);
+        setCopyActive(true);
+        setTimeout(() => setCopyActive(false), 2000);
+      }}
+    >
+      {copyActive ? <ClipboardCheck className="text-success" /> : <Clipboard />}
+    </Button>
+  );
+};
