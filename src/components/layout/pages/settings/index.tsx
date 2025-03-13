389d249aa23311ef1e6349bbdad0666434b9a9f0
Sasha Khamkov <sanusart@gmail.com>
2025-03-13 08:24:19 +0200

    feat(responsiveness): responsive views adjustments

--- src/components/layout/pages/settings/index.tsx
+++ src/components/layout/pages/settings/index.tsx
@@ -45,8 +45,7 @@ export const Settings = ({ isCollapsed = false, setIsCollapsed = () => {} }: Pro
       </PageHeader>
 
       <PageContent>
-        {/*<pre>{JSON.stringify(settings, null, 2)}</pre>*/}
-        <div className="grid grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
           <Card>
             <CardHeader>
               <CardTitle>Settings</CardTitle>
