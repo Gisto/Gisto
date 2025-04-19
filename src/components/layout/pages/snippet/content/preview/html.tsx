5d75a067eb62c86a2f67d4b0dc3efe8faed872ee
Sasha Khamkov <sanusart@gmail.com>
2025-04-19 15:14:16 +0300

    fix(preview): previews height
    fix #482

--- src/components/layout/pages/snippet/content/preview/html.tsx
+++ src/components/layout/pages/snippet/content/preview/html.tsx
@@ -1,5 +1,7 @@
 import { GistFileType } from '@/types/gist.ts';
 
 export const Html = ({ file }: { file: GistFileType }) => (
-  <iframe className="w-full h-full border-none" sandbox="" srcDoc={file.content} />
+  <div className="max-h-65dvh h-65dvh">
+    <iframe className="w-full h-full border-none" sandbox="" srcDoc={file.content} />
+  </div>
 );
